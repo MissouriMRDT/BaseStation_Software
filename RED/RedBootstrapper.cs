e2aedebc27926197a481f60480fc7ef9f0d576e8
Ty Morrow <tjm6f4@mst.edu>
2014-11-04 10:07:41 -0600

    Removed unnecessary StartupStatusView.

--- RED/RedBootstrapper.cs
+++ RED/RedBootstrapper.cs
@@ -1,10 +1,8 @@
 ﻿namespace RED
 {
-	using System.Threading;
 	using Caliburn.Micro;
 	using System.Windows;
 	using ViewModels;
-	using Views;
 
 	public class RedBootstrapper : BootstrapperBase
 	{
@@ -15,14 +13,6 @@
 
 		protected override void OnStartup(object sender, StartupEventArgs e)
 		{
-			//Disable shutdown when the dialog closes
-			Application.Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
-			var dialog = new StartupStatusView();
-			dialog.Show();
-			// Do initialization work here...
-			dialog.Close();
-			Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
-
 			DisplayRootViewFor<ShellViewModel>();
 		}
 	}
