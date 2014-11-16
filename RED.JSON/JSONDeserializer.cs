ab3e477c17af034447b202cf00e41ddc78833aeb
Josh Reed <jlrdkc@mst.edu>
2014-11-15 22:22:10 -0600

    Implemented JSON Deserializer Wrapper

---
+++ RED.JSON/JSONDeserializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RED.JSON
+{
+    public static class JSONDeserializer
+    {
+        public static async T Deserialize<T>(string value)
+        {
+            return await JsonConvert.DeserializeObjectAsync<T>(value);
+        }
+    }
+}
