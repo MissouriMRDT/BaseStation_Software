96a4cc1e67174a3cccc78093177ba62a92fd5ad3
Josh Reed <jlrdkc@mst.edu>
2014-11-15 22:29:54 -0600

    Refactor JSONDeserializer

--- RED.JSON/JSONDeserializer.cs
+++ RED.JSON/JSONDeserializer.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Newtonsoft.Json;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace RED.JSON
 {
     public static class JSONDeserializer
     {
-        public static async T Deserialize<T>(string value)
+        public static async Task<T> Deserialize<T>(string value)
         {
             return await JsonConvert.DeserializeObjectAsync<T>(value);
         }
