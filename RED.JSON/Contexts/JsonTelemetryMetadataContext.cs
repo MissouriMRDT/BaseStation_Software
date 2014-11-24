1ce17ed9d88ed173de24fd6eabbaaecbf60d2cfc
Josh Reed <jlrdkc@mst.edu>
2014-11-24 16:40:49 -0600

    Added JsonTelemetryMetadataContext Class

---
+++ RED.JSON/Contexts/JsonTelemetryMetadataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace RED.JSON.Contexts
+{
+    public class JsonTelemetryMetadataContext
+    {
+        public byte Telem_ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Datatype { get; set; }
+        public string Units { get; set; }
+        public string Minimum { get; set; }
+        public string Maximum { get; set; }
+    }
+}
