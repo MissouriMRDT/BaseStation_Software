371bd38c2e84946eda770b18f8c4e929526a80f5
Josh Reed <jlrdkc@mst.edu>
2014-11-25 15:26:26 -0600

    Cleaned up Usings in MetadataContexts

--- RED/Contexts/TelemetryMetadataContext.cs
+++ RED/Contexts/TelemetryMetadataContext.cs
@@ -1,10 +1,5 @@
 ﻿using RED.Interfaces;
 using RED.JSON.Contexts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RED.Contexts
 {
