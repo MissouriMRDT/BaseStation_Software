b9c2ca7df457ba17971591ca2e69dde47dd48fbd
JakePickle <jmp6xb@mst.edu>
2015-02-28 16:14:52 -0600

    Connected views to view models

--- REDUpdater/Views/ConsoleView.xaml.cs
+++ REDUpdater/Views/ConsoleView.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using REDUpdater.ViewModels;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace REDUpdater.Views
 {
@@ -23,6 +11,7 @@ namespace REDUpdater.Views
         public ConsoleView()
         {
             InitializeComponent();
+            this.DataContext = new ConsoleViewModel();
         }
     }
 }
