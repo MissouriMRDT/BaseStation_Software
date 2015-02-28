358266d22d6471b855fe19574454f850b3525962
JakePickle <jmp6xb@mst.edu>
2015-02-28 16:16:46 -0600

    Created model fields in view models

--- REDUpdater/ViewModels/ConsoleViewModel.cs
+++ REDUpdater/ViewModels/ConsoleViewModel.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using REDUpdater.Models;
 
 namespace REDUpdater.ViewModels
 {
     class ConsoleViewModel
     {
+        ConsoleModel Model = new ConsoleModel();
     }
 }
