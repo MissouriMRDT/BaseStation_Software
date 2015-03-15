de40e9327324bbff534547444330a456ef4e4606
JakePickle <jmp6xb@mst.edu>
2015-03-15 17:59:48 -0500

    created internal fields in Models

--- REDUpdater/Models/ConsoleModel.cs
+++ REDUpdater/Models/ConsoleModel.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace REDUpdater.Models
 {
     class ConsoleModel
     {
+        internal string text = string.Empty;
     }
 }
