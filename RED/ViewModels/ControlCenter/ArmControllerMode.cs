7818d58e82496b60af27bf45fb1f539b9493fb68
Josh Reed <jlrdkc@mst.edu>
2015-03-21 01:42:24 -0500

    Added ArmControllerMode Class

---
+++ RED/ViewModels/ControlCenter/ArmControllerMode.cs
@@ -0,0 +1,39 @@
+﻿using RED.Interfaces;
+using RED.ViewModels.ControlCenter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RED.ViewModels.ControlCenter
+{
+    public class ArmControllerMode : IControllerMode
+    {
+        private readonly ControlCenterViewModel _controlCenter;
+
+        public string Name { get; set; }
+        public InputViewModel InputVM { get; set; }
+
+        public ArmControllerMode(InputViewModel inputVM, ControlCenterViewModel cc)
+        {
+            InputVM = inputVM;
+            _controlCenter = cc;
+        }
+
+        public void EnterMode()
+        {
+
+        }
+
+        public void EvaluateMode()
+        {
+
+        }
+
+        public void ExitMode()
+        {
+
+        }
+    }
+}
