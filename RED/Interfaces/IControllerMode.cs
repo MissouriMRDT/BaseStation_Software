d923e598c1af70460817e67fae1060fd0df3b92e
Josh Reed <jlrdkc@mst.edu>
2015-03-20 23:55:55 -0500

    Added IControllerMode Interface

---
+++ RED/Interfaces/IControllerMode.cs
@@ -0,0 +1,14 @@
+﻿using RED.ViewModels.ControlCenter;
+
+namespace RED.Interfaces
+{
+    public interface IControllerMode
+    {
+        string Name { get; set; }
+        InputViewModel InputVM { get; set; }
+
+        void EnterMode();
+        void EvaluateMode();
+        void ExitMode();
+    }
+}
