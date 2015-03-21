de45a6119251f8431bb1b8f2f537079ef2e1f9dc
Josh Reed <jlrdkc@mst.edu>
2015-03-21 11:24:10 -0500

    Added GripperControllerMode Class

---
+++ RED/ViewModels/ControlCenter/GripperControllerMode.cs
@@ -0,0 +1,39 @@
+﻿using RED.Interfaces;
+using RED.ViewModels.ControlCenter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RED.ViewModels.ControlCenter
+{
+    public class GripperControllerMode : IControllerMode
+    {
+        private readonly ControlCenterViewModel _controlCenter;
+
+        public string Name { get; set; }
+        public InputViewModel InputVM { get; set; }
+
+        public GripperControllerMode(InputViewModel inputVM, ControlCenterViewModel cc)
+        {
+            InputVM = inputVM;
+            _controlCenter = cc;
+        }
+
+        public void EnterMode()
+        {
+
+        }
+
+        public void EvaluateMode()
+        {
+
+        }
+
+        public void ExitMode()
+        {
+
+        }
+    }
+}
