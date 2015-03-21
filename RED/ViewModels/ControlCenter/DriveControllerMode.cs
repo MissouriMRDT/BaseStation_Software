e06ce116ccc4eb27af5b3930fd8820fcd380bd21
Josh Reed <jlrdkc@mst.edu>
2015-03-21 00:31:57 -0500

    Added DriveControllerMode Class

---
+++ RED/ViewModels/ControlCenter/DriveControllerMode.cs
@@ -0,0 +1,43 @@
+﻿using RED.Interfaces;
+using RED.ViewModels.ControlCenter;
+using SharpDX.XInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RED.ViewModels.ControlCenter
+{
+    public class DriveControllerMode : IControllerMode
+    {
+        private readonly ControlCenterViewModel _controlCenter;
+
+        private int speedLeft;
+        private int speedRight;
+
+        public string Name { get; set; }
+        public InputViewModel InputVM { get; set; }
+
+        public DriveControllerMode(InputViewModel inputVM, ControlCenterViewModel cc)
+        {
+            InputVM = inputVM;
+            _controlCenter = cc;
+        }
+
+        public void EnterMode()
+        {
+
+        }
+
+        public void EvaluateMode()
+        {
+
+        }
+
+        public void ExitMode()
+        {
+
+        }
+    }
+}
