427b24787402214de30d775962c2d6e0788fb14e
Josh Reed <jlrdkc@mst.edu>
2015-03-21 11:26:37 -0500

    Ported Gripper code from InputVM

--- RED/ViewModels/ControlCenter/GripperControllerMode.cs
+++ RED/ViewModels/ControlCenter/GripperControllerMode.cs
@@ -1,5 +1,6 @@
 ﻿using RED.Interfaces;
 using RED.ViewModels.ControlCenter;
+using SharpDX.XInput;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,9 @@ namespace RED.ViewModels.ControlCenter
         public string Name { get; set; }
         public InputViewModel InputVM { get; set; }
 
+        private const int OPEN = 0;
+        private const int CLOSE = 1;
+
         public GripperControllerMode(InputViewModel inputVM, ControlCenterViewModel cc)
         {
             InputVM = inputVM;
@@ -28,7 +32,19 @@ namespace RED.ViewModels.ControlCenter
 
         public void EvaluateMode()
         {
+            Controller c = InputVM.ControllerOne;
+            if (c != null && !c.IsConnected) return;
 
+            if (InputVM.ButtonB)
+            {
+                _controlCenter.DataRouter.Send(_controlCenter.MetadataManager.GetCommand("AuxGripper").Id, CLOSE);
+                return;
+            }
+            if (InputVM.ButtonX)
+            {
+                _controlCenter.DataRouter.Send(_controlCenter.MetadataManager.GetCommand("AuxGripper").Id, OPEN);
+                return;
+            }
         }
 
         public void ExitMode()
