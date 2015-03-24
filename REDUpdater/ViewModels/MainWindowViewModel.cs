99a89d55da3e61d496a6fbc6fb6283877aa6d937
JakePickle <jmp6xb@mst.edu>
2015-03-24 12:01:17 -0500

    copied code for prototype updater into REDUpdater

--- REDUpdater/ViewModels/MainWindowViewModel.cs
+++ REDUpdater/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,11 @@
 ﻿using REDUpdater.Models;
 using Caliburn.Micro;
+using Octokit;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace REDUpdater.ViewModels
 {
@@ -38,6 +44,72 @@ namespace REDUpdater.ViewModels
             Model = new MainWindowModel();
             Model.console = new ConsoleViewModel(this);
             Model.percent = 0;
+
+            const string originalFolder = "Debug";//folder RED.exe is in WARNING!!! ALL FILES IN THIS DIRECTORY WILL BE DELETED
+            string dir = System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase.Substring(8);
+            string running = System.AppDomain.CurrentDomain.FriendlyName;
+            string zipPath, delDir;
+            string[] zipList, delList;
+
+            dir = dir.Substring(0, dir.Length - 1);
+            while (dir[dir.Length - 1] != '/')
+            {
+                dir = dir.Substring(0, dir.Length - 1);
+            }
+
+            Task.Run(async () =>
+            {
+                var github = new GitHubClient(new ProductHeaderValue("name"));//Creates link to github
+
+                var releases = await github.Release.GetAll("JakePickle", "GitHubUpdateTest");//gets all releases of RED   
+
+                delDir = dir.Substring(0, dir.Length - 1);
+
+                while (delDir[delDir.Length - 1] != '/')
+                {
+                    delDir = delDir.Substring(0, delDir.Length - 1);
+                }
+
+                delDir = delDir + originalFolder + "/";
+
+                delList = Directory.GetFiles(delDir);
+
+                foreach (string f in delList)
+                {
+                    File.Delete(f);
+                }
+
+                percent = 20;
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile("https://github.com/MST-MRDT/Rover-Engagement-Display/releases/download/" + releases[0].TagName + "/1.1.0.zip",
+                    dir + releases[0].TagName + ".zip");
+
+                percent = 50
+                    ;
+                zipPath = dir + releases[0].TagName + ".zip";
+
+                ZipFile.ExtractToDirectory(zipPath, delDir);
+
+                percent = 75;
+
+                zipList = Directory.GetFiles(dir, "*.zip");
+
+                foreach (string f in zipList)
+                {
+                    File.Delete(f);
+                }
+
+                percent = 95;
+
+                Process updateTest = new Process();
+                updateTest.StartInfo.FileName = delDir + "RED.exe";
+                updateTest.Start();
+
+                percent = 100;
+
+                System.Environment.Exit(1);
+            });
         }
     }
 }
