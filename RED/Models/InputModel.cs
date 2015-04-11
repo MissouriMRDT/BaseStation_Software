e7d1ecd5beb9ab2861ed1e48d74db2035cda4e39
Josh Reed <jlrdkc@mst.edu>
2015-04-10 20:04:15 -0500

    Reversed Bugfix for Drive Testing

--- RED/Models/InputModel.cs
+++ RED/Models/InputModel.cs
@@ -5,7 +5,7 @@ namespace RED.Models
 {
     internal class InputModel
     {
-        internal int SerialReadSpeed = 1000;
+        internal int SerialReadSpeed = 30;
 
         internal ObservableCollection<IControllerMode> ControllerModes = new ObservableCollection<IControllerMode>();
         internal int CurrentModeIndex;
