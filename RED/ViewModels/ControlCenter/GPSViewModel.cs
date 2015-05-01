cf66f749fd45b0e7f3d777f96ae1e534e4683f7e
Josh Reed <jlrdkc@mst.edu>
2015-04-30 20:06:51 -0500

    Added Files for GPS Component

---
+++ RED/ViewModels/ControlCenter/GPSViewModel.cs
@@ -0,0 +1,21 @@
+﻿using RED.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RED.ViewModels.ControlCenter
+{
+    public class GPSViewModel
+    {
+        GPSModel _model;
+        ControlCenterViewModel _cc;
+
+        public GPSViewModel(ControlCenterViewModel cc)
+        {
+            _model = new GPSModel();
+            _cc = cc;
+        }
+    }
+}
