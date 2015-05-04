2defb8012d10452c1c76e14d76453a8556eb741a
Josh Reed <jlrdkc@mst.edu>
2015-05-03 21:54:30 -0500

    Added Default Values for Manual Deadzone

--- RED/Models/InputModel.cs
+++ RED/Models/InputModel.cs
@@ -6,8 +6,8 @@ namespace RED.Models
     internal class InputModel
     {
         internal int SerialReadSpeed = 30;
-        internal int ManualDeadzone;
-        internal bool AutoDeadzone;
+        internal int ManualDeadzone = 50;
+        internal bool AutoDeadzone = false;
 
         internal ObservableCollection<IControllerMode> ControllerModes = new ObservableCollection<IControllerMode>();
         internal int CurrentModeIndex;
