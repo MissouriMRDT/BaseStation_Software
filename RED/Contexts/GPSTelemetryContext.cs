9816039738a94d746781dc91301288aecaa04e1b
Josh Reed <jlrdkc@mst.edu>
2015-05-05 20:15:45 -0500

    Added GPSTelemetryContext Struct

---
+++ RED/Contexts/GPSTelemetryContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RED.Contexts
+{
+    public struct GPSTelemetryContext
+    {
+        bool Fix;
+        int Longitude;
+        int Latitude;
+        float Altitude;
+        float Speed;
+        float Angle;
+        byte FixQuality;
+        byte NumberOfSatellites;
+    }
+}
