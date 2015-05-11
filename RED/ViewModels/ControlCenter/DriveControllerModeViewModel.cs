9c191b361ae161769d453c47d8b5145f8d5c82fe
Josh Reed <jlrdkc@mst.edu>
2015-05-10 19:45:12 -0500

    Added Drive Stop on Drive Mode Exit

--- RED/ViewModels/ControlCenter/DriveControllerModeViewModel.cs
+++ RED/ViewModels/ControlCenter/DriveControllerModeViewModel.cs
@@ -139,7 +139,8 @@ namespace RED.ViewModels.ControlCenter
 
         public void ExitMode()
         {
-
+            _controlCenter.DataRouter.Send(_controlCenter.MetadataManager.GetId("MotorLeftSpeed"), 0);
+            _controlCenter.DataRouter.Send(_controlCenter.MetadataManager.GetId("MotorRightSpeed"), 0);
         }
     }
 }
