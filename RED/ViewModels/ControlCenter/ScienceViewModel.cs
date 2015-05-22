bc4fc656f6f162c517ff71fb228e0584b668dce5
Josh Reed <jlrdkc@mst.edu>
2015-05-22 13:04:18 -0500

    Added Basic Science Classes

---
+++ RED/ViewModels/ControlCenter/ScienceViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using RED.Models;
+using RED.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RED.ViewModels.ControlCenter
+{
+    public class ScienceViewModel : PropertyChangedBase
+    {
+        private ScienceModel _model;
+        private ControlCenterViewModel _cc;
+
+        public ScienceViewModel(ControlCenterViewModel cc)
+        {
+            _model = new ScienceModel();
+            _cc = cc;
+        }
+    }
+}
