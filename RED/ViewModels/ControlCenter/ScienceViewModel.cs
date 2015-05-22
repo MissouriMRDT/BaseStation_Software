621f2cb949b710a178edc5768e05cd771a19ce49
Josh Reed <jlrdkc@mst.edu>
2015-05-22 14:23:47 -0500

    Implemented Science Data Receiving

--- RED/ViewModels/ControlCenter/ScienceViewModel.cs
+++ RED/ViewModels/ControlCenter/ScienceViewModel.cs
@@ -1,18 +1,23 @@
 ﻿using Caliburn.Micro;
+using RED.Interfaces;
 using RED.Models;
 using RED.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RED.ViewModels.ControlCenter
 {
-    public class ScienceViewModel : PropertyChangedBase
+    public class ScienceViewModel : PropertyChangedBase, ISubscribe
     {
         private ScienceModel _model;
         private ControlCenterViewModel _cc;
+
+        private short[] CCDPixelBuffer;
+        private short CCDPixelsRecieved;
 
         public float Ph
         {
@@ -92,6 +97,67 @@ namespace RED.ViewModels.ControlCenter
         {
             _model = new ScienceModel();
             _cc = cc;
+        }
+
+        public void RequestData()
+        {
+            StartCCDReceive();
+            _cc.DataRouter.Send(_cc.MetadataManager.GetId("ScienceRequest"), 0);
+            _cc.Console.WriteToConsole("Science data requested.");
+        }
+
+        public void ReceiveFromRouter(byte dataId, byte[] data)
+        {
+            switch (_cc.MetadataManager.GetTelemetry(dataId).Name)
+            {
+                case "Ph":
+                    Ph = BitConverter.ToSingle(data, 0);
+                    break;
+                case "Moisture":
+                    Moisture = BitConverter.ToInt16(data, 0);
+                    break;
+                case "CCD":
+                    if (!CCDIsReceiving)
+                    {
+                        _cc.Console.WriteToConsole("Unexpected CCD data received. Packet number " + data[0].ToString());
+                        break;
+                    }
+
+                    byte packetNum = data[0];
+                    int index = packetNum * 12;
+                    for (int i = 1; i < data.Length; i += 2)
+                    {
+                        short pixel = BitConverter.ToInt16(data, i);
+                        CCDPixelBuffer[index++] = pixel;
+                        CCDPixelsRecieved++;
+                    }
+
+                    CCDProgress = (float)CCDPixelsRecieved / CCDPixelCount;
+
+                    if (CCDPixelsRecieved == CCDPixelCount)
+                        FinishCCDReceive();
+
+                    break;
+            }
+        }
+
+        public void StartCCDReceive()
+        {
+            CCDIsReceiving = true;
+            CCDProgress = 0;
+        }
+
+        private void FinishCCDReceive()
+        {
+            CCDIsReceiving = false;
+            CCDProgress = 1.0f;
+
+            //Save CSV File
+            string csv = String.Join(",", CCDPixelBuffer);
+            string filename = "REDScienceData" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".csv";
+            string path = Path.Combine(CCDFilePath, filename);
+            File.WriteAllText(path, csv);
+            _cc.Console.WriteToConsole("CCD data saved to file " + path + ".");
         }
     }
 }
