25e40131a53d8ac50eff69a217dfe8ba5829274d
Josh Reed <jlrdkc@mst.edu>
2015-07-24 19:25:52 -0500

    Added Basic Sensor Classes

---
+++ RED/ViewModels/ControlCenter/SensorViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+using RED.Models;
+using RED.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RED.ViewModels.ControlCenter
+{
+    public class SensorViewModel : PropertyChangedBase, ISubscribe
+    {
+        private SensorModel _model;
+        private ControlCenterViewModel _cc;
+
+        public SensorViewModel(ControlCenterViewModel cc)
+        {
+            _model = new SensorModel();
+            _cc = cc;
+        }
+
+        public void ReceiveFromRouter(byte dataId, byte[] data)
+        {
+
+        }
+    }
+}
