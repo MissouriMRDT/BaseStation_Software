e3edd66b607394030b90a1ff4713fe1734a1453d
Josh Reed <jlrdkc@mst.edu>
2015-11-01 15:42:19 -0600

    Implemented INetworkEncoding

---
+++ RED/Interfaces/INetworkEncoding.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RED.Interfaces
+{
+    public interface INetworkEncoding
+    {
+        byte[] EncodePacket(byte dataId, byte[] data);
+        byte[] DecodePacket(byte[] data, out byte dataId);
+    }
+}
