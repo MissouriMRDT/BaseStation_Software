4a73d4a3b5d2f6ac1a9edb14d22a932cc3454a93
Josh Reed <jlrdkc@mst.edu>
2015-11-01 15:31:19 -0600

    Implemented Network Manager

---
+++ RED/ViewModels/ControlCenter/NetworkManagerViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Caliburn.Micro;
+using RED.Models;
+using RED.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RED.ViewModels.ControlCenter
+{
+    public class NetworkManagerViewModel : PropertyChangedBase, ISubscribe
+    {
+        private NetworkManagerModel _model;
+        private ControlCenterViewModel _cc;
+
+        public NetworkManagerViewModel(ControlCenterViewModel cc)
+        {
+            _model = new NetworkManagerModel();
+            _cc = cc;
+
+            foreach (var command in _cc.MetadataManager.Commands)
+                _cc.DataRouter.Subscribe(this, command.Id);
+        }
+
+        public void ReceiveFromRouter(byte dataId, byte[] data)
+        {
+            //TODO: send data over network
+        }
+    }
+}
