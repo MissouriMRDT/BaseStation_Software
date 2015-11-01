2942dc17d29746bb6828b5bafa7494a1cd6d5e1c
Josh Reed <jlrdkc@mst.edu>
2015-11-01 17:17:22 -0600

    Implemented UDPEndpoint

---
+++ RED/ViewModels/ControlCenter/UDPEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using RED.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RED.ViewModels.ControlCenter
+{
+    public class UDPEndpoint : INetworkTransportProtocol
+    {
+        private UdpClient client;
+        private ushort destPort;
+
+        public UDPEndpoint(ushort destPort)
+        {
+            client = new UdpClient();
+            this.destPort = destPort;
+        }
+
+        public async Task SendMessage(IPAddress destIP, byte[] data)
+        {
+            await client.SendAsync(data, data.Length, new IPEndPoint(destIP, destPort));
+        }
+
+        public async Task<byte[]> ReceiveMessage()
+        {
+            return (await client.ReceiveAsync()).Buffer;
+        }
+    }
+}
