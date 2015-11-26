ea0fbad73acbf46455609ab17b2af6eff8de21e8
Josh Reed <jlrdkc@mst.edu>
2015-11-25 23:25:38 -0600

    Implemented SubscriptionManagerVM

--- RED/ViewModels/ControlCenter/SubscriptionManagerViewModel.cs
+++ RED/ViewModels/ControlCenter/SubscriptionManagerViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using RED.Addons;
+using RED.Interfaces;
 using RED.Models;
 using RED.ViewModels;
 using System;
@@ -12,8 +13,11 @@ namespace RED.ViewModels.ControlCenter
 {
     public class SubscriptionManagerViewModel
     {
+        public const byte SubscriptionDataId = 2;
+
         private SubscriptionManagerModel _model;
         private ControlCenterViewModel _cc;
+        private IIPAddressProvider ipProvider;
 
         public Dictionary<byte, SubscriptionRecord> Subscriptions
         {
@@ -27,6 +31,34 @@ namespace RED.ViewModels.ControlCenter
         {
             _model = new SubscriptionManagerModel();
             _cc = cc;
+            ipProvider = _cc.MetadataManager;
+        }
+
+        public void Subscribe(byte dataId)
+        {
+            var ip = ipProvider.GetIPAddress(dataId);
+            _cc.NetworkManager.SendPacket(SubscriptionDataId, BitConverter.GetBytes(dataId), ip);
+            Subscriptions.Add(dataId, new SubscriptionRecord(SubscriptionStatus.Subscribed, ip, DateTime.Now));
+        }
+
+        public void Unsubscribe(byte dataId)
+        {
+            _cc.NetworkManager.SendPacket(SubscriptionDataId, BitConverter.GetBytes(dataId), Subscriptions[dataId].HostIP);
+            Subscriptions[dataId].Status = SubscriptionStatus.Unsubscribed;
+            Subscriptions[dataId].Timestamp = DateTime.Now;
+        }
+
+        public void Resubscribe(byte dataId)
+        {
+            var ip = ipProvider.GetIPAddress(dataId);
+            _cc.NetworkManager.SendPacket(SubscriptionDataId, BitConverter.GetBytes(dataId), ip);
+            Subscriptions[dataId].Timestamp = DateTime.Now;
+        }
+
+        public void Resubscribe(IPAddress ip)
+        {
+            foreach (var sub in Subscriptions.Where(x => x.Value.HostIP == ip))
+                Resubscribe(sub.Key);
         }
     }
 
