e5490b1ab588d3f34789a520bb29c43dcbbf6528
Josh Reed <jlrdkc@mst.edu>
2015-11-25 22:27:41 -0600

    Added SubscriptionManager

---
+++ RED/ViewModels/ControlCenter/SubscriptionManagerViewModel.cs
@@ -0,0 +1,22 @@
+﻿using RED.Models;
+using RED.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RED.ViewModels.ControlCenter
+{
+    public class SubscriptionManagerViewModel
+    {
+        private SubscriptionManagerModel _model;
+        private ControlCenterViewModel _cc;
+
+        public SubscriptionManagerViewModel(ControlCenterViewModel cc)
+        {
+            _model = new SubscriptionManagerModel();
+            _cc = cc;
+        }
+    }
+}
