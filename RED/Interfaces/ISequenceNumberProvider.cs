4339f3d58d513a26d7e19ae1d5ea7eb6d69694a9
Josh Reed <jlrdkc@mst.edu>
2015-11-27 15:40:05 -0600

    Added ISequenceNumberProvider

---
+++ RED/Interfaces/ISequenceNumberProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RED.Interfaces
+{
+    public interface ISequenceNumberProvider
+    {
+        ushort GetValue(byte dataId);
+        void SetValue(byte dataId, ushort value);
+        void ClearValue(byte dataId);
+        ushort IncrementValue(byte dataId);
+        bool UpdateNewer(byte dataId, ushort value);
+        bool UpdateConsecutive(byte dataId, ushort value);
+    }
+}
