61c65b22a0e1ccf6355a50226bbf703f9254effd
Josh Reed <jlrdkc@mst.edu>
2016-01-11 15:55:28 -0600

    Implemented Sending Reliable Packets
    Including Retransmitting

--- RED/ViewModels/ControlCenter/NetworkManagerViewModel.cs
+++ RED/ViewModels/ControlCenter/NetworkManagerViewModel.cs
@@ -13,6 +13,10 @@ namespace RED.ViewModels.ControlCenter
     public class NetworkManagerViewModel : PropertyChangedBase, ISubscribe
     {
         private const ushort DestinationPort = 11000;
+        private const bool defaultReliable = true;
+
+        private TimeSpan ReliableRetransmissionTimeout { get { return TimeSpan.FromMilliseconds(1000); } }
+        private int ReliableMaxRetries { get { return 5; } }
 
         private NetworkManagerModel _model;
         private ControlCenterViewModel _cc;
@@ -21,6 +25,8 @@ namespace RED.ViewModels.ControlCenter
         private INetworkTransportProtocol continuousDataSocket;
         private IIPAddressProvider ipAddressProvider;
         private ISequenceNumberProvider sequenceNumberProvider;
+
+        private HashSet<UnACKedPacket> OutgoingUnACKed = new HashSet<UnACKedPacket>();
 
         public NetworkManagerViewModel(ControlCenterViewModel cc)
         {
@@ -55,16 +61,41 @@ namespace RED.ViewModels.ControlCenter
             SendPacket(dataId, data, destIP);
         }
 
-        public async void SendPacket(byte dataId, byte[] data, IPAddress destIP)
+        public void SendPacket(byte dataId, byte[] data, IPAddress destIP, bool isReliable)
         {
             if (destIP == null)
             {
                 _cc.Console.WriteToConsole("Attempted to send packet with unknown IP address. DataId=" + dataId.ToString());
                 return;
             }
+
             ushort seqNum = sequenceNumberProvider.IncrementValue(dataId);
-            byte[] packet = encoding.EncodePacket(dataId, data, seqNum);
-            await continuousDataSocket.SendMessage(destIP, packet);
+            byte[] packetData = encoding.EncodePacket(dataId, data, seqNum);
+
+            if (isReliable)
+                SendPacketReliable(destIP, packetData, dataId, seqNum);
+            else
+                SendPacketUnreliable(destIP, packetData);
+        }
+
+        private async void SendPacketUnreliable(IPAddress destIP, byte[] packetData)
+        {
+            await continuousDataSocket.SendMessage(destIP, packetData);
+        }
+
+        private async void SendPacketReliable(IPAddress destIP, byte[] packetData, byte dataId, ushort seqNum)
+        {
+            var packetInfo = new UnACKedPacket() { DataId = dataId, SeqNum = seqNum };
+            OutgoingUnACKed.Add(packetInfo);
+
+            for (int i = 0; i < ReliableMaxRetries; i++)
+            {
+                await continuousDataSocket.SendMessage(destIP, packetData);
+                await Task.Delay(ReliableRetransmissionTimeout);
+                if (!OutgoingUnACKed.Contains(packetInfo)) return;
+            }
+
+            _cc.Console.WriteToConsole("No ACK recieved for reliable packet with DataId=" + packetInfo.DataId.ToString() + " and SeqNum=" + packetInfo.SeqNum.ToString() + " after " + ReliableMaxRetries + " retries.");
         }
 
         private void ReceivePacket(byte[] buffer)
@@ -79,5 +110,7 @@ namespace RED.ViewModels.ControlCenter
             }
             _cc.DataRouter.Send(dataId, data);
         }
+
+        private struct UnACKedPacket { public byte DataId; public ushort SeqNum;}
     }
 }
