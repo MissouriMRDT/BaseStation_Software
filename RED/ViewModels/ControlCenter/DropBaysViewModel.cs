44e4fd42ac1620d58d347ac3266a65f597e5cfc6
Josh Reed <jlrdkc@mst.edu>
2016-04-17 12:02:02 -0500

    Added DropBaysVM

---
+++ RED/ViewModels/ControlCenter/DropBaysViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Caliburn.Micro;
+using RED.Models;
+using RED.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RED.ViewModels.ControlCenter
+{
+    public class DropBaysViewModel : PropertyChangedBase
+    {
+        private ControlCenterViewModel _cc;
+
+        public DropBaysViewModel(ControlCenterViewModel cc)
+        {
+            _cc = cc;
+        }
+    }
+}
