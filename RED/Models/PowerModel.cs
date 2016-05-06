0a691acea1759969a07af9a9baa9819396790ef0
Josh Reed <jlrdkc@mst.edu>
2016-05-06 15:05:37 -0500

    Added PowerVM

---
+++ RED/Models/PowerModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RED.Models
+{
+    public class PowerModel
+    {
+        internal float Motor1Current;
+        internal float Motor2Current;
+        internal float Motor3Current;
+        internal float Motor4Current;
+        internal float Motor5Current;
+        internal float Motor6Current;
+        internal float Motor7Current;
+        internal float Motor8Current;
+        internal float Bus5VCurrent;
+        internal float Bus12VCurrent;
+        internal float InputVoltage;
+
+        internal float Cell1Voltage;
+        internal float Cell2Voltage;
+        internal float Cell3Voltage;
+        internal float Cell4Voltage;
+        internal float Cell5Voltage;
+        internal float Cell6Voltage;
+        internal float Cell7Voltage;
+        internal float Cell8Voltage;
+        internal float TotalPackCurrent;
+        internal float TotalPackVoltage;
+    }
+}
