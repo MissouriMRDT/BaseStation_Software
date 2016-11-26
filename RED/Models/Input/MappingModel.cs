bfbcdf5abf35a276dbf4896a90386d99091d3bac
Josh Reed <jlrdkc@mst.edu>
2016-11-26 14:08:30 -0600

    Added Mapping Classes

---
+++ RED/Models/Input/MappingModel.cs
@@ -0,0 +1,14 @@
+﻿using RED.ViewModels.Input;
+using System.Collections.ObjectModel;
+
+namespace RED.Models.Input
+{
+    internal class MappingModel
+    {
+        internal ObservableCollection<MappingChannelViewModel> Channels;
+        internal string DeviceType;
+        internal string ModeType;
+        internal uint UpdatePeriod;
+        internal bool IsActive;
+    }
+}
---
+++ RED/ViewModels/Input/MappingViewModel.cs
@@ -0,0 +1,93 @@
+﻿using Caliburn.Micro;
+using RED.Models.Input;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace RED.ViewModels.Input
+{
+    public class MappingViewModel : PropertyChangedBase
+    {
+        private MappingModel _model;
+
+        public ObservableCollection<MappingChannelViewModel> Channels
+        {
+            get
+            {
+                return _model.Channels;
+            }
+            set
+            {
+                _model.Channels = value; NotifyOfPropertyChange(() => Channels);
+            }
+
+        }
+        public string DeviceType
+        {
+            get
+            {
+                return _model.DeviceType;
+            }
+            set
+            {
+                _model.DeviceType = value; NotifyOfPropertyChange(() => DeviceType);
+            }
+
+        }
+        public string ModeType
+        {
+            get
+            {
+                return _model.ModeType;
+            }
+            set
+            {
+                _model.ModeType = value; NotifyOfPropertyChange(() => ModeType);
+            }
+
+        }
+        public uint UpdatePeriod
+        {
+            get
+            {
+                return _model.UpdatePeriod;
+            }
+            set
+            {
+                _model.UpdatePeriod = value; NotifyOfPropertyChange(() => UpdatePeriod);
+            }
+
+        }
+        public bool IsActive
+        {
+            get
+            {
+                return _model.IsActive;
+            }
+            set
+            {
+                _model.IsActive = value; NotifyOfPropertyChange(() => IsActive);
+            }
+
+        }
+
+        public MappingViewModel()
+        {
+            _model = new MappingModel();
+        }
+
+        public void Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Dictionary<string, float> Map(Dictionary<string, float> values)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
