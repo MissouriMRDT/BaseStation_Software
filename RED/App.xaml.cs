36ac967633c931f290066190a0a29183ea676841
Josh Reed <jlrdkc@mst.edu>
2017-03-25 14:44:06 -0500

    Implemented Unhandled Exception Reporting

--- RED/App.xaml.cs
+++ RED/App.xaml.cs
@@ -1,7 +1,15 @@
-﻿namespace RED
-{
-	public partial class App
-	{
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
 
-	}
+namespace RED
+{
+    public partial class App
+    {
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs args)
+        {
+            Clipboard.SetText(args.Exception.ToString());
+            MessageBox.Show("An unhandled exception has occurred in RED. The following details have been copied to the clipboard." + Environment.NewLine + Environment.NewLine + args.Exception.ToString(), "RED Unhandled Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
 }
