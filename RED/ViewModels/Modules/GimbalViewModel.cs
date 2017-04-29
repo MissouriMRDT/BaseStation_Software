ebab5c2b085623b86e07679b8fba8b88b6456037
Josh Reed <jlrdkc@mst.edu>
2017-04-29 16:55:20 -0500

    Added Support for Gimbal 2
    This will need to be fixed once full input controller support is
    implemented.

--- RED/ViewModels/Modules/GimbalViewModel.cs
+++ RED/ViewModels/Modules/GimbalViewModel.cs
@@ -54,7 +54,7 @@ namespace RED.ViewModels.Modules
             _log = log;
             InputVM = inputVM;
             Name = "Gimbal " + (gimbalIndex + 1).ToString();
-            ModeType = "Gimbal";
+            ModeType = "Gimbal " + (gimbalIndex + 1).ToString();
             SpeedLimit = 1000;
             GimbalIndex = gimbalIndex;
         }
