8fb9602fb6e7aaceceefa881a945f89ad2802dd3
Josh Reed <jlrdkc@mst.edu>
2017-05-30 19:23:55 -0600

    Refactored GMapControl to MapVM

--- RED/ViewModels/Navigation/MapViewModel.cs
+++ RED/ViewModels/Navigation/MapViewModel.cs
@@ -1,7 +1,15 @@
-﻿using Caliburn.Micro;
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsPresentation;
+using Caliburn.Micro;
 using RED.Addons;
 using RED.Models.Navigation;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Media;
+using System.Windows;
+using System.Collections.Generic;
+using System.Windows.Shapes;
 
 namespace RED.ViewModels.Navigation
 {
@@ -34,12 +42,109 @@ namespace RED.ViewModels.Navigation
             }
         }
 
+        private GMapControl mainMap = new GMapControl();
+        public GMapControl MainMap
+        {
+            get
+            {
+                return mainMap;
+            }
+        }
+
         public MapViewModel()
         {
             _model = new MapModel();
+            InitializeMapControl();
+
             Waypoints.Add(new GPSCoordinate(37.951631, -91.777713)); //Rolla
             Waypoints.Add(new GPSCoordinate(37.850025, -91.701845)); //Fugitive Beach
             Waypoints.Add(new GPSCoordinate(38.406426, -110.791919)); //Mars Desert Research Station
+            RefreshMap();
+        }
+
+        private void InitializeMapControl()
+        {
+            MainMap.Margin = new Thickness(-5);
+
+            MainMap.Manager.Mode = AccessMode.CacheOnly;
+            MainMap.MapProvider = GMapProviders.OpenStreetMapQuestHybrid;
+            MainMap.Position = new PointLatLng(RED.Properties.Settings.Default.GPSStartLocationLatitude, RED.Properties.Settings.Default.GPSStartLocationLongitude);
+            MainMap.MinZoom = 1;
+            MainMap.MaxZoom = 18;
+            MainMap.Zoom = 10;
+
+            MainMap.EmptyMapBackground = Brushes.White;
+            MainMap.EmptytileBrush = Brushes.White;
+            MainMap.EmptyTileText = new FormattedText("No imagery available in cache.", System.Globalization.CultureInfo.CurrentUICulture, FlowDirection.LeftToRight, new Typeface("Sans Serif"), SystemFonts.MessageFontSize, Brushes.Black);
+
+            MainMap.IgnoreMarkerOnMouseWheel = true;
+            MainMap.MouseWheelZoomType = MouseWheelZoomType.MousePositionWithoutCenter;
+            //MainMap.FillEmptyTiles = false; //Good for debugging map cache
+        }
+
+        public void CacheImport()
+        {
+            MainMap.ShowImportDialog();
+        }
+        public void CacheExport()
+        {
+            MainMap.ShowExportDialog();
+        }
+        public void CachePrefetch()
+        {
+            RectLatLng area = MainMap.SelectedArea;
+            if (!area.IsEmpty)
+            {
+                for (int i = (int)MainMap.Zoom; i <= MainMap.MaxZoom; i++)
+                {
+                    MessageBoxResult res = MessageBox.Show("Ready ripp at Zoom = " + i + " ?", "GMap.NET", MessageBoxButton.YesNoCancel);
+
+                    if (res == MessageBoxResult.Yes)
+                    {
+                        TilePrefetcher obj = new TilePrefetcher();
+                        obj.Owner = Application.Current.MainWindow;
+                        obj.ShowCompleteMessage = true;
+                        obj.Start(area, i, MainMap.MapProvider, 100);
+                    }
+                    else if (res == MessageBoxResult.No)
+                    {
+                        continue;
+                    }
+                    else if (res == MessageBoxResult.Cancel)
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Select map area holding ALT", "GMap.NET", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+        public void CacheClear()
+        {
+            if (MessageBox.Show("Are You sure?", "Clear GMap.NET cache?", MessageBoxButton.OKCancel, MessageBoxImage.Warning) == MessageBoxResult.OK)
+            {
+                try
+                {
+                    MainMap.Manager.PrimaryCache.DeleteOlderThan(System.DateTime.Now, null);
+                    MessageBox.Show("Done. Cache is clear.");
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        public void RefreshMap()
+        {
+            MainMap.Markers.Clear();
+
+            var converter = new RED.Addons.GMapMarkerCollectionMultiConverter();
+            var newdata = (IEnumerable<GMapMarker>)converter.Convert(new object[] { CurrentLocation, Waypoints }, typeof(System.Collections.ObjectModel.ObservableCollection<GMapMarker>), null, System.Globalization.CultureInfo.DefaultThreadCurrentUICulture);
+            foreach (var marker in newdata)
+                MainMap.Markers.Add(marker);
         }
     }
 }
--- RED/Views/Navigation/MapView.xaml.cs
+++ RED/Views/Navigation/MapView.xaml.cs
@@ -1,12 +1,4 @@
-﻿using GMap.NET;
-using GMap.NET.MapProviders;
-using GMap.NET.WindowsPresentation;
-using RED.ViewModels.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
+﻿using System.Windows.Controls;
 
 namespace RED.Views.Navigation
 {
@@ -18,84 +10,6 @@ namespace RED.Views.Navigation
         public MapView()
         {
             InitializeComponent();
-            
-            MainMap.Manager.Mode = AccessMode.CacheOnly;
-            MainMap.MapProvider = GMapProviders.OpenStreetMapQuestHybrid;
-            MainMap.Position = new PointLatLng(RED.Properties.Settings.Default.GPSStartLocationLatitude, RED.Properties.Settings.Default.GPSStartLocationLongitude);
-            MainMap.Zoom = 10.0;
-
-            MainMap.EmptyMapBackground = Brushes.White;
-            MainMap.EmptytileBrush = Brushes.White;
-            MainMap.EmptyTileText = new FormattedText("No imagery available in cache.", System.Globalization.CultureInfo.CurrentUICulture, FlowDirection.LeftToRight, new Typeface("Sans Serif"), SystemFonts.MessageFontSize, Brushes.Black);
-
-            MainMap.IgnoreMarkerOnMouseWheel = true;
-            MainMap.MouseWheelZoomType = MouseWheelZoomType.MousePositionWithoutCenter;
-            //MainMap.FillEmptyTiles = false; //Good for debugging map cache
-        }
-
-        private void ImportBtn_Click(object sender, RoutedEventArgs e)
-        {
-            MainMap.ShowImportDialog();
-        }
-        private void ExportBtn_Click(object sender, RoutedEventArgs e)
-        {
-            MainMap.ShowExportDialog();
-        }
-        private void PrefetchBtn_Click(object sender, RoutedEventArgs e)
-        {
-            RectLatLng area = MainMap.SelectedArea;
-            if (!area.IsEmpty)
-            {
-                for (int i = (int)MainMap.Zoom; i <= MainMap.MaxZoom; i++)
-                {
-                    MessageBoxResult res = MessageBox.Show("Ready ripp at Zoom = " + i + " ?", "GMap.NET", MessageBoxButton.YesNoCancel);
-
-                    if (res == MessageBoxResult.Yes)
-                    {
-                        TilePrefetcher obj = new TilePrefetcher();
-                        obj.Owner = Application.Current.MainWindow;
-                        obj.ShowCompleteMessage = true;
-                        obj.Start(area, i, MainMap.MapProvider, 100);
-                    }
-                    else if (res == MessageBoxResult.No)
-                    {
-                        continue;
-                    }
-                    else if (res == MessageBoxResult.Cancel)
-                    {
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                MessageBox.Show("Select map area holding ALT", "GMap.NET", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-            }
-        }
-        private void ClearBtn_Click(object sender, RoutedEventArgs e)
-        {
-            if (MessageBox.Show("Are You sure?", "Clear GMap.NET cache?", MessageBoxButton.OKCancel, MessageBoxImage.Warning) == MessageBoxResult.OK)
-            {
-                try
-                {
-                    MainMap.Manager.PrimaryCache.DeleteOlderThan(DateTime.Now, null);
-                    MessageBox.Show("Done. Cache is clear.");
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-            }
-        }
-
-        private void RefreshMap_Click(object sender, RoutedEventArgs e)
-        {
-            var converter = new RED.Addons.GMapMarkerCollectionMultiConverter();
-            var vm = (MapViewModel)DataContext;
-            MainMap.Markers.Clear();
-            var newdata = (IEnumerable<GMapMarker>)converter.Convert(new object[] { vm.CurrentLocation, vm.Waypoints }, typeof(System.Collections.ObjectModel.ObservableCollection<GMapMarker>), null, System.Globalization.CultureInfo.DefaultThreadCurrentUICulture);
-            foreach (var marker in newdata)
-                MainMap.Markers.Add(marker);
         }
     }
 }
