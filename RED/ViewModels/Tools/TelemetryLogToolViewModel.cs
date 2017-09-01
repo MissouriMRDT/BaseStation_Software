3c03a9a6ea187397696538940c2318e73630e162
Josh Reed <jlrdkc@mst.edu>
2017-08-31 23:02:20 -0500

    Refactored TelemetryLog Model

--- RED/ViewModels/Tools/TelemetryLogToolViewModel.cs
+++ RED/ViewModels/Tools/TelemetryLogToolViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Caliburn.Micro;
 using RED.Addons.Network;
 using RED.Interfaces.Network;
+using RED.Models.Tools;
 using RED.ViewModels.Network;
 using System;
 using System.Collections.ObjectModel;
@@ -12,12 +13,25 @@ namespace RED.ViewModels.Tools
 {
     public class TelemetryLogToolViewModel : PropertyChangedBase
     {
+        private readonly TelemetryLogToolModel _model;
         private readonly IServerProvider _serverProvider;
 
-        public ObservableCollection<ServerLog> TelemetryLog { get; set; }
+        public ObservableCollection<ServerLog> TelemetryLog
+        {
+            get
+            {
+                return _model.TelemetryLog;
+            }
+            private set
+            {
+                _model.TelemetryLog = value;
+                NotifyOfPropertyChange(() => TelemetryLog);
+            }
+        }
 
         public TelemetryLogToolViewModel(NetworkManagerViewModel networkVM, IServerProvider serverProvider)
         {
+            _model = new TelemetryLogToolModel();
             _serverProvider = serverProvider;
 
             TelemetryLog = new ObservableCollection<ServerLog>(_serverProvider.GetServerList().Select(x => new ServerLog(x)));
@@ -33,16 +47,48 @@ namespace RED.ViewModels.Tools
 
         public class ServerLog : PropertyChangedBase
         {
-            private string _name;
-            private IPAddress _address;
-            private DateTime _timestamp;
+            private readonly TelemetryLogToolModel.ServerLogModel _model;
 
-            public string Name { get { return _name; } set { _name = value; NotifyOfPropertyChange(() => Name); } }
-            public IPAddress Address { get { return _address; } set { _address = value; NotifyOfPropertyChange(() => Address); } }
-            public DateTime Timestamp { get { return _timestamp; } set { _timestamp = value; NotifyOfPropertyChange(() => Timestamp); } }
+            public string Name
+            {
+                get
+                {
+                    return _model.Name;
+                }
+                set
+                {
+                    _model.Name = value; NotifyOfPropertyChange(() => Name);
+                }
+
+            }
+            public IPAddress Address
+            {
+                get
+                {
+                    return _model.Address;
+                }
+                set
+                {
+                    _model.Address = value; NotifyOfPropertyChange(() => Address);
+                }
+
+            }
+            public DateTime Timestamp
+            {
+                get
+                {
+                    return _model.Timestamp;
+                }
+                set
+                {
+                    _model.Timestamp = value; NotifyOfPropertyChange(() => Timestamp);
+                }
+
+            }
 
             public ServerLog(Server srv)
             {
+                _model = new TelemetryLogToolModel.ServerLogModel();
                 Name = srv.Name;
                 Address = srv.Address;
                 Timestamp = DateTime.MinValue;
