1abde926d5db127538aa432cf6d443cfcf9e2986
Nathan Skelton <nathan.hunter.skelton@gmail.com>
2018-04-14 15:06:08 -0500

    Added logging for arm

--- RED/ViewModels/Modules/ArmViewModel.cs
+++ RED/ViewModels/Modules/ArmViewModel.cs
@@ -81,6 +81,7 @@ namespace RED.ViewModels.Modules
             set
             {
                 _model.ControlState = value;
+                _log.Log(ControlState);
                 NotifyOfPropertyChange(() => ControlState);
             }
         }
@@ -307,6 +308,7 @@ namespace RED.ViewModels.Modules
                     AngleJ4 = BitConverter.ToSingle(data, 3 * sizeof(float));
                     AngleJ5 = BitConverter.ToSingle(data, 4 * sizeof(float));
                     AngleJ6 = BitConverter.ToSingle(data, 5 * sizeof(float));
+                    _log.Log("Recieved Arm Positions");
                     break;
                 case "ArmFault":
                     _log.Log($"Arm reported a fault code of {data[0]}");
