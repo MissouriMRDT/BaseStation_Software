15a1e2ee0ea7daf629095eaa3d94d72c4bd6baaa
Nathan Skelton <nathan.hunter.skelton@gmail.com>
2018-09-19 16:22:23 -0500

    Added caliburn & metro, framework for app

---
+++ RoverNetworkManager/RNMBootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using RoverNetworkManager.ViewModels;
+using System.Windows;
+
+namespace RoverNetworkManager
+{
+    public class RNMBootstrapper : BootstrapperBase
+    {
+        public RNMBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainWindowViewModel>();
+        }
+    }
+}
---
+++ RoverNetworkManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Caliburn.Micro;
+using RoverNetworkManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverNetworkManager.ViewModels
+{
+    public class MainWindowViewModel : Screen
+    {
+        private readonly MainWindowModel _model;
+
+        public MainWindowViewModel()
+        {
+            base.DisplayName = "Rover Network Manager";
+            _model = new MainWindowModel();
+        }
+    }
+}
