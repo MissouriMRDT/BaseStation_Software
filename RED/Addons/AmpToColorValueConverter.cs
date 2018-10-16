d0083f6572a5e7f82fe77bb06020846319824bd2
Nathan Skelton <nathan.hunter.skelton@gmail.com>
2018-10-16 18:01:24 -0500

    Sample boolean behavior for amp value

---
+++ RED/Addons/AmpToColorValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace RED.Addons
+{
+    public class AmpToColorValueConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if((float)value > 1.0)
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
+            return new SolidColorBrush(Colors.Green);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
