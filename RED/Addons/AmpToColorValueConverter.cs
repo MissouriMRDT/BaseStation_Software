734139b7ec5821e73a582a08a82b149831537858
ps2dude7 <ps2dude67@sbcglobal.net>
2018-10-17 17:09:22 -0500

    Added cutoff as variable
    Slight change allows for easier modification in the future, and should assure that  no matter what the cutoff is, the intensity multiplier is always 255 when value = cutoff.

--- RED/Addons/AmpToColorValueConverter.cs
+++ RED/Addons/AmpToColorValueConverter.cs
@@ -15,14 +15,16 @@ namespace RED.Addons
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             int max = 15;
-            if((float)value > 1.0)  //a value of 0, the worst, most red scenario, should give 255, 0, 0, the most red color
+            double cutoff = 1.0;
+
+            if((float)value > cutoff)  //a value of 0, the worst, most red scenario, should give 255, 0, 0, the most red color
             {                       //a value of 1, the "best", least great color, should give 255, 255, 255, which is white.
                 byte intensity = (byte)(((max - (float)value)) * (255/max));
                 return new SolidColorBrush(Color.FromRgb(255, intensity, intensity));
             }
             else                    //a value of 0, the best, most green scenario, should give 0, 255, 0, the most green color
             {                       //a value of 1, the "worst", least great color, should give 255, 255, 255, which is white.
-                byte intensity = (byte)((((float)value)) * 255);
+                byte intensity = (byte)((((float)value)) * 255/cutoff);
                 return new SolidColorBrush(Color.FromRgb(intensity, 255, intensity));
             }
             
