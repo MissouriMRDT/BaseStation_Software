9cb6e0f9b6278708bf250cf1f2fa0844d75c80e7
ps2dude7 <ps2dude67@sbcglobal.net>
2018-10-17 19:39:56 -0500

    Broken Style Version
    wip change colors based off a style we pass the bind into

--- RED/Addons/AmpToColorValueConverter.cs
+++ RED/Addons/AmpToColorValueConverter.cs
@@ -11,19 +11,20 @@ namespace RED.Addons
 {
     public class AmpToColorValueConverter : IValueConverter
     {
+        
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             int max = 15;
             double cutoff = 1.0;
-
+            
             if((float)value > cutoff)  //a value of 0, the worst, most red scenario, should give 255, 0, 0, the most red color
-            {                       //a value of 1, the "best", least great color, should give 255, 255, 255, which is white.
+            {                          //a value of 1, the "best", least great color, should give 255, 255, 255, which is white.
                 byte intensity = (byte)(((max - (float)value)) * (255/max));
                 return new SolidColorBrush(Color.FromRgb(255, intensity, intensity));
             }
-            else                    //a value of 0, the best, most green scenario, should give 0, 255, 0, the most green color
-            {                       //a value of 1, the "worst", least great color, should give 255, 255, 255, which is white.
+            else                       //a value of 0, the best, most green scenario, should give 0, 255, 0, the most green color
+            {                          //a value of 1, the "worst", least great color, should give 255, 255, 255, which is white.
                 byte intensity = (byte)((((float)value)) * 255/cutoff);
                 return new SolidColorBrush(Color.FromRgb(intensity, 255, intensity));
             }
