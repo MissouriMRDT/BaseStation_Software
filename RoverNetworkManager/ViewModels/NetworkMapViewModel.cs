f26849236f3f7bad304b4a952eb73a4f8ebecf02
Nathan Skelton <nathan.hunter.skelton@gmail.com>
2018-10-27 14:59:08 -0500

    Revised network map to have structure

---
+++ RoverNetworkManager/ViewModels/NetworkMapViewModel.cs
@@ -0,0 +1,22 @@
+﻿using RoverNetworkManager.Models;
+using RoverNetworkManager.Networking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using System.Windows.Controls;
+
+namespace RoverNetworkManager.ViewModels
+{
+    public class NetworkMapViewModel : PropertyChangedBase
+    {
+        private readonly NetworkMapModel _model;
+
+        public NetworkMapViewModel()
+        {
+            _model = new NetworkMapModel();
+		}
+	}
+}
--- RoverNetworkManager/Views/NetworkMapView.xaml.cs
+++ RoverNetworkManager/Views/NetworkMapView.xaml.cs
@@ -1,23 +1,15 @@
-﻿namespace RoverNetworkManager.Views
+﻿using System.Windows.Controls;
+
+namespace RoverNetworkManager.Views
 {
     /// <summary>
     /// Interaction logic for MainWindowView.xaml
     /// </summary>
-    public partial class MainWindowView
+    public partial class NetworkMapView : UserControl
     {
-        public MainWindowView()
+        public NetworkMapView()
         {
             InitializeComponent();
-        }
-
-        private void m_testing_Unselected(object sender, System.Windows.RoutedEventArgs e)
-        {
-
-        }
-
-        private void m_testing_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
-        {
-
         }
     }
 }
