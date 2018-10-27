035a9106f59cbc6a553ae6da05bc8c5ca2fca0e9
Arpan Das <admmm@mst.edu>
2018-10-27 10:06:47 -0500

    My map (work in progress)

---
+++ RoverNetworkManager/Views/NetworkMapView.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace RoverNetworkManager.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindowView.xaml
+    /// </summary>
+    public partial class MainWindowView
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+        }
+
+        private void m_testing_Unselected(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void m_testing_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+
+        }
+    }
+}
