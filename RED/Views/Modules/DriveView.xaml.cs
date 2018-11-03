21bea8d5de2f8abed8b5b7c68bb48f9a96276da0
Matt Montgomery <mmznc@mst.edu>
2018-11-03 14:20:53 -0500

    Add camera feed to driveview

--- RED/Views/Modules/DriveView.xaml.cs
+++ RED/Views/Modules/DriveView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.IO;
+using System.Windows.Controls;
 
 namespace RED.Views.Modules
 {
@@ -10,6 +11,27 @@ namespace RED.Views.Modules
         public DriveView()
         {
             InitializeComponent();
+			SetupVLC();
         }
-    }
+
+		internal void SetupVLC() {
+			var vlcLibDirectory = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "libvlc", System.IntPtr.Size == 4 ? "win-x86" : "win-x64"));
+
+			var options = new string[]
+			{
+				":network-caching=0",
+				"--rtsp-caching=0"
+				 //"--file-logging", "-vvv", "--extraintf=logger", "--logfile=vlc.log"
+			};
+
+			vlcPlayer.MediaPlayer.VlcLibDirectory = vlcLibDirectory;
+			vlcPlayer.MediaPlayer.VlcMediaplayerOptions = options;
+			vlcPlayer.MediaPlayer.EndInit();
+			PlayChannel(1);
+		}
+
+		public void PlayChannel(int channel) {
+			vlcPlayer.MediaPlayer.Play($"rtsp://admin:Rovin2012@192.168.1.226:554/mpeg4/ch0{channel}/main/av_stream");
+		}
+	}
 }
