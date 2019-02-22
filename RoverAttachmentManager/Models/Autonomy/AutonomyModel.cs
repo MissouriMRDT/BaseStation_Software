fa4355a88f41fb5c303e0280f9ac29e12b8125b2
Michael Pieper <michaelepieper@gmail.com>
2019-02-21 19:43:17 -0600

    Implement autonomy view model

--- RoverAttachmentManager/Models/Autonomy/AutonomyModel.cs
+++ RoverAttachmentManager/Models/Autonomy/AutonomyModel.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RoverAttachmentManager.Models.Autonomy
+﻿namespace RoverAttachmentManager.Models.Autonomy
 {
-    class AutonomyModel
+    internal class AutonomyModel
     {
     }
 }
