1cce5b30533cd9ef35131de94323d36fa7aa4f76
Matt Montgomery <33811686+ConfusedPolarBear@users.noreply.github.com>
2019-04-11 15:00:21 -0500

    Fix ping tool IP addresses

--- RoverNetworkManager/Configurations/PingToolConfig.cs
+++ RoverNetworkManager/Configurations/PingToolConfig.cs
@@ -9,14 +9,15 @@
             new PingServerContext("Base Station 5Ghz Rocket", "192.168.1.85", true, false),
             new PingServerContext("Rover 5Ghz Rocket", "192.168.1.84", true, false),
             new PingServerContext("Rover 2.4Mhz Rocket", "192.168.1.86", true, false),
-            new PingServerContext("Drive Board", "192.168.1.130", true, true),
+            new PingServerContext("Drive Board", "192.168.1.134", true, true),
             new PingServerContext("Arm Board", "192.168.1.131", true, true),
             new PingServerContext("Power Board", "192.168.1.132", true, true),
-            new PingServerContext("Nav Board", "192.168.1.133", true, true),
-            new PingServerContext("Gimbal Board", "192.168.1.134", true, true),
-            new PingServerContext("Science Board", "192.168.1.135", true, true),
-            new PingServerContext("Autonomy Board", "192.168.1.138", true, true),
-            new PingServerContext("Camera Board", "192.168.1.140", true, true),
+            new PingServerContext("Nav Board", "192.168.1.136", true, true),
+            new PingServerContext("Gimbal Board", "192.168.1.135", true, true),
+			new PingServerContext("Actuation Science Board", "192.168.1.137", true, true),
+			new PingServerContext("Sensor Science Board", "192.168.1.138", true, true),
+            new PingServerContext("Autonomy Board", "192.168.1.139", true, true),
+            new PingServerContext("Camera Board", "192.168.1.136", true, true),
             new PingServerContext("Grandstream", "192.168.1.226", true, false)
         });
     }
