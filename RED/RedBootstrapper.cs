39ad0c2a94547943fd76b021fa2d7b779e1d0eb3
Benjamin <benjaminro21@gmail.com>
2019-09-29 20:01:29 -0500

    Changes Filepath
    Changes the absoltue filepath into a relative filepath, as C:/Users/Benjamin Rowe/ Probably doesn't exist on Base Station.

--- RED/RedBootstrapper.cs
+++ RED/RedBootstrapper.cs
@@ -1,5 +1,6 @@
 ﻿using Caliburn.Micro;
 using RED.ViewModels;
+using System;
 using System.Windows;
 
 namespace RED
@@ -14,6 +15,11 @@ namespace RED
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ControlCenterViewModel>();
+        }
+
+        protected override void OnExit(object sender, EventArgs e)
+        {
+            System.IO.File.WriteAllText(System.IO.Path.GetFullPath("RoverMetrics.txt"), "end");
         }
     }
 }
