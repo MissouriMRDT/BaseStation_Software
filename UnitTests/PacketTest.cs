a0671af8a020e7601031c188f293867ff999a338
Nathan Skelton <nathan.hunter.skelton@gmail.com>
2019-10-05 14:27:39 -0500

    Updated all tests to test byte structure for little endian

--- UnitTests/PacketTest.cs
+++ UnitTests/PacketTest.cs
@@ -14,9 +14,18 @@ namespace UnitTests
             Assert.AreEqual(name, packet.Name, "Name Incorrect");
             Assert.AreEqual(count, packet.Count, "Count Incorrect");
             Assert.AreEqual(type, packet.DataType, "DataType Incorrect");
-            Assert.AreEqual(data, packet.GetData<byte>(), "Data Incorrect");
+            Assert.AreEqual(data, packet.GetData<T>(), "Data Incorrect");
         }
-        
+
+        private void CheckPacket<T>(Packet packet, string name, int count, byte type, string raw , T data)
+        {
+            Assert.AreEqual(name, packet.Name, "Name Incorrect");
+            Assert.AreEqual(count, packet.Count, "Count Incorrect");
+            Assert.AreEqual(raw, BitConverter.ToString(packet.Data), "Raw Data Incorrect");
+            Assert.AreEqual(type, packet.DataType, "DataType Incorrect");
+            Assert.AreEqual(data, packet.GetData<T>(), "Data Incorrect");
+        }
+
         [TestMethod]
         public void Data_Null()
         {
@@ -31,8 +40,7 @@ namespace UnitTests
         {
             SByte data = -3;
             Packet packet = Packet.Create("Hi", data);
-
-            CheckPacket<SByte>(packet, "Hi", 1, 0, data);
+            CheckPacket<SByte>(packet, "Hi", 1, 0, "FD", data);
         }
 
         [TestMethod]
@@ -41,16 +49,16 @@ namespace UnitTests
             Byte data = 3;
             Packet packet = Packet.Create("Hi", data);
 
-            CheckPacket<Byte>(packet, "Hi", 1, 1, data);
+            CheckPacket<Byte>(packet, "Hi", 1, 1, "03", data);
         }
 
         [TestMethod]
         public void Data_Int16()
         {
-            Int16 data = 200;
+            Int16 data = -200;
             Packet packet = Packet.Create("Hi", data);
 
-            CheckPacket<Int16>(packet, "Hi", 1, 2, data);
+            CheckPacket<Int16>(packet, "Hi", 1, 2, "38-FF", data);
         }
 
         [TestMethod]
@@ -59,34 +67,34 @@ namespace UnitTests
             UInt16 data = 200;
             Packet packet = Packet.Create("Hi", data);
 
-            CheckPacket<UInt16>(packet, "Hi", 1, 3, data);
+            CheckPacket<UInt16>(packet, "Hi", 1, 3, "C8-00", data);
         }
 
         [TestMethod]
         public void Data_Int32()
         {
-            Int32 data = 200;
+            Int32 data = -123000;
             Packet packet = Packet.Create("Hi", data);
 
-            CheckPacket<Int32>(packet, "Hi", 1, 4, data);
+            CheckPacket<Int32>(packet, "Hi", 1, 4, "88-1F-FE-FF", data);
         }
 
         [TestMethod]
         public void Data_UInt32()
         {
-            UInt32 data = 200;
+            UInt32 data = 123000;
             Packet packet = Packet.Create("Hi", data);
 
-            CheckPacket<UInt32>(packet, "Hi", 1, 5, data);
+            CheckPacket<UInt32>(packet, "Hi", 1, 5, "78-E0-01-00", data);
         }
 
         [TestMethod]
         public void Data_Int64()
         {
-            Int64 data = 200000;
+            Int64 data = -123000;
             Packet packet = Packet.Create("Hi", data);
 
-            CheckPacket<Int64>(packet, "Hi", 1, 6, data);
+            CheckPacket<Int64>(packet, "Hi", 1, 6, "88-1F-FE-FF-FF-FF-FF-FF", data);
         }
 
     }
