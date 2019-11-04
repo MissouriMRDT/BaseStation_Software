735f97be3ba1c2f6bb8d12f211599e3ee8972510
ps2dude7 <ps2dude67@sbcglobal.net>
2019-11-03 18:03:44 -0600

    Create files
    Create new files for the control multipliers in the RAM-Arm folder. Import all the same things we were importing in the RAM-Arm files.

---
+++ RoverAttachmentManager/ViewModels/Arm/ControlMultipliersViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using Core.Interfaces;
+using Core.Models;
+using Core.RoveProtocol;
+using Core.ViewModels.Input;
+using RoverAttachmentManager.Configurations.Modules;
+using RoverAttachmentManager.Contexts;
+using RoverAttachmentManager.Models.ArmModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+
+namespace RoverAttachmentManager.ViewModels.Arm
+{
+    public class ControlMultipliersViewModel : PropertyChangedBase
+    {
+        public ControlMultipliersViewModel()
+        {
+        }
+    }
+}
