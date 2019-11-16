2c3c839b8b34ace8aaefdf1ceacffdfc36071775
Nathan Skelton <nathan.hunter.skelton@gmail.com>
2019-11-15 18:05:00 -0600

    Update comments in IRovecomm

--- Core/Interfaces/IRovecomm.cs
+++ Core/Interfaces/IRovecomm.cs
@@ -4,43 +4,41 @@ using Core.RoveProtocol;
 namespace Core.Interfaces
 {
     /// <summary>
-    /// The rove communication protocol interface; it offers a variety of api for sending and receiving messages across the network 
-    /// using the rovecomm protocol. Most devices used on rover use rovecomm, so use this to communicate with its devices. Rovecomm itself will encode 
-    /// and decode messages between Core and devices and pass it over the network.
+    /// The rove communication protocol interface.
+    /// It offers a api for sending and receiving messages across the network using the rovecomm protocol. 
+    /// Most devices used on rover use rovecomm, so use this to communicate with its devices. Rovecomm 
+    /// implementations will reference an encoder/decoder to encode and decode messages from the network.
     /// </summary>
     public interface IRovecomm
     {
 
-		/// <summary>
-		/// send a rovecomm message over the network. Again. Internal overload since the ip addresses are baked into the dataid, so only
-		/// certain rovecomm-system messages should call it or the network manager sends a packet with a custom ip address.
-		/// </summary>
-		/// <param name="dataId">dataid of the message to send</param>
-		/// <param name="data">data to send</param>
-		/// <param name="destIP">ip of the device to send the message to</param>
-		/// <param name="reliable">whether to send it reliably (IE with a non broadcast protocol) or not</param>
-		void SendCommand(Packet packet, bool reliable = false, IPAddress destIP = null);
+        /// <summary>
+        /// Send a rovecomm message over the network.
+        /// </summary>
+        /// <param name="packet">Packet of the message to send</param>
+        /// <param name="reliable">Whether to send it reliably (TCP) or not (UDP)</param>
+        /// <param name="destIP">IP of the device to send the message to</param>
+        void SendCommand(Packet packet, bool reliable = false, IPAddress destIP = null);
 
 		/// <summary>
-		/// request to be notified whenever a rovecomm message comes in from the network carrying the 
-		/// corresponding dataid, which is based on rovecomm metadata id's
+		/// Request for view models to be notified whenever a rovecomm message comes in from the network 
+        /// carrying the corresponding dataid
 		/// </summary>
-		/// <param name="receiver">the receiver to be notified (the class should input itself as this)</param>
-		/// <param name="dataId">the data id of the message you want to be notified of</param>
+		/// <param name="receiver">Receiver to be notified (typically, "this")</param>
+		/// <param name="dataId">Data id of the message you want to be notified of</param>
 		void NotifyWhenMessageReceived(IRovecommReceiver receiver, string dataName);
 
         /// <summary>
-        /// request to subscribe this computer to a device on the network so that it will send this pc rovecomm
+        /// Request to subscribe to a device on the network so that it will send this pc rovecomm
         /// messages. This must be called before any rovecomm streams can be received by this computer.
         /// </summary>
-        /// <param name="deviceIP">the ip address of the device to request</param>
+        /// <param name="deviceIP">IP address of the device to request</param>
         void SubscribeTo(IPAddress deviceIP);
 
         /// <summary>
-        /// request to subscribe this computer to all devices on the network so that they will send this pc rovecomm
+        /// Request to subscribe to all devices on the network so that they will send this pc rovecomm
         /// messages. This must be called before any rovecomm streams can be received by this computer.
         /// </summary>
-        /// <param name="deviceIP">the ip address of the device to request</param>
         void SubscribeToAll();
     }
 }
