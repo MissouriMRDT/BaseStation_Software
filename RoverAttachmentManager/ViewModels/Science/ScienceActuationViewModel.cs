5d6adefb44c963b91e5b5b934598c4ec9fd903d6
thomas-gleiforst <ps2dude67@gmail.com>
2020-01-19 19:34:08 -0600

    Created files for Actuation

---
+++ RoverAttachmentManager/ViewModels/Science/ScienceActuationViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using Core.Interfaces;
+using Core.Models;
+using Core.RoveProtocol;
+using OxyPlot;
+using OxyPlot.Annotations;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using OxyPlot.Wpf;
+using RoverAttachmentManager.Models.Science;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace RoverAttachmentManager.ViewModels.Science
+{
+    public class ScienceActuationViewModel : PropertyChangedBase, IRovecommReceiver
+    {
+
+    }
+}
