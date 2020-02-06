c430f6c827fc54df28521bfd034fcfe3ac3b6a56
richelle schwarz <chelle.warz@gmail.com>
2020-02-06 16:12:52 -0600

    telemetry feature
    telemetry feature

---
+++ RoverAttachmentManager/Models/Autonomy/AutonomyTelemetryModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverAttachmentManager.Models.Autonomy
+{
+    internal class AutonomyTelemetryModel
+    {
+        internal string CurrentState;
+        internal string TBD;
+        internal short Left;
+        internal short Right;
+        internal float CurrentLatitude;
+        internal float CurrentLongitude;
+        internal float TargetLatitude;
+        internal float TargetLongitude;
+        internal float Pitch;
+        internal float Roll;
+        internal float Heading;
+        internal float TargetHeading;
+        internal float LiDAR;
+    }
+}
---
+++ RoverAttachmentManager/ViewModels/Autonomy/AutonomyTelemetryViewModel.cs
@@ -0,0 +1,111 @@
+﻿using Caliburn.Micro;
+using Core.Interfaces;
+using Core.Models;
+using RoverAttachmentManager.Models.Autonomy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverAttachmentManager.ViewModels.Autonomy
+{
+    public class AutonomyTelemetryViewModel: PropertyChangedBase, IRovecommReceiver
+    {
+        private readonly AutonomyTelemetryModel _model;
+        private readonly IRovecomm _rovecomm;
+        private readonly IDataIdResolver _idResolver;
+        private readonly ILogger _logger;
+
+        public string CurrentState
+        {
+            get
+            {
+                return _model.CurrentState;
+            }
+            set
+            {
+                _model.CurrentState = value;
+                NotifyOfPropertyChange(() => CurrentState);
+            }
+        public string TBD
+         {
+            get
+            {
+                return _model.TBD;
+            }
+            set
+            {
+                _model.TBD = value;
+                NotifyOfPropertyChange(() => TBD);
+            }
+        public short Left
+        {
+            get
+            {
+                return _model.Left;
+            }
+            set
+            {
+                _model.TBD = value;
+                NotifyOfPropertyChange(() => Left);
+            }
+        public short Right
+        {
+            get
+            {
+                return _model.Right;
+            }
+            set
+            {
+                _model.TBD = value;
+                NotifyOfPropertyChange(() => Right);
+            }
+        public float CurrentLatitude
+        {
+            get
+            {
+                return _model.CurrentLatitude;
+            }
+            set
+            {
+                _model.TBD = value;
+                NotifyOfPropertyChange(() => CurrentLatitude);
+            }
+        public float CurrentLongitude
+        {
+            get
+            {
+                return _model.CurrentLongitude;
+            }
+            set
+            {
+                _model.TBD = value;
+                NotifyOfPropertyChange(() => CurrentLongitude);
+            }
+            //continue.
+
+        public AutonomyTelemetryViewModel(IRovecomm networkMessenger, IDataIdResolver idResolver, ILogger logger)
+        {
+            _model = new AutonomyTelemetryModel();
+            _rovecomm = networkMessenger;
+            _idResolver = idResolver;
+            _logger = logger;
+        }
+
+        public void ReceivedRovecommMessageCallback(Packet packet, bool reliable)
+        {
+            switch (packet.Name)
+            {
+                case "WaypointReached":
+                    _logger.Log("Waypoint Reached");
+                    break;
+            }
+        }
+
+        public void ReceivedRovecommMessageCallback(int index, bool reliable)
+        {
+            ReceivedRovecommMessageCallback(_rovecomm.GetPacketByID(index), false);
+        }
+    }
+}
