d95bd56bf059ceede756c963e6e04ae307451170
thomas-gleiforst <ps2dude67@gmail.com>
2020-02-12 10:29:51 -0600

    Made Stopwatch more adaptabile
    Removed margins, set progress bars to fill container instead of having fixed width, changed the width of all columns to * (so they are dynamic but the same), and made the long button texts wrap.

--- RED/Views/Tools/StopwatchToolView.xaml.cs
+++ RED/Views/Tools/StopwatchToolView.xaml.cs
@@ -13,19 +13,6 @@ namespace RED.Views.Tools
             InitializeComponent();
         }
 
-    /*    private void FixTime_Click(object sender, RoutedEventArgs e)
-        {
-            FixContextMenu.IsEnabled = true;
-            FixContextMenu.PlacementTarget = (Button)sender;
-            FixContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
-            FixContextMenu.IsOpen = true;
-        }
-
-        private void SetTime_Click(object sender, RoutedEventArgs e)
-        {
-            FixContextMenu.IsOpen = false;
-        }*/
-
         private void EditTime_Click(object sender, RoutedEventArgs e)
         {
             EditContextMenu.IsEnabled = true;
