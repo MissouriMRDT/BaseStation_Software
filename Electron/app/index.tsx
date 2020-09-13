2d80bab24cd109e909af38f209f21dbe1487e57f
thomas-gleiforst <ps2dude67@gmail.com>
2020-09-12 21:02:20 -0500

    missed linter fix

--- Electron/app/index.tsx
+++ Electron/app/index.tsx
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react'
-import { render } from 'react-dom'
-import { AppContainer as ReactHotAppContainer } from 'react-hot-loader'
-import GPS from './components/GPS'
-import App from './containers/App'
+import React, { Fragment } from "react"
+import { render } from "react-dom"
+import { AppContainer as ReactHotAppContainer } from "react-hot-loader"
+import GPS from "./components/GPS"
+import App from "./containers/App"
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // eslint-disable-next-line global-require
   // const Root = require('./containers/Root').default
   render(
@@ -15,6 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
         <GPS />
       </App>
     </AppContainer>,
-    document.getElementById('root')
+    document.getElementById("root")
   )
 })
