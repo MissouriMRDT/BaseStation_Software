f17e6c15ef1cf7a3de49378e0a8c52055ed98557
thomas-gleiforst <ps2dude67@gmail.com>
2020-09-27 16:21:59 -0500

    Fix migration/merge issues

---
+++ WPF/RED/Models/CameraModel.cs
@@ -0,0 +1,12 @@
+﻿using Core.Cameras;
+using System.Windows.Controls;
+
+namespace RED.Models
+{
+    internal class CameraModel
+    {
+        internal Camera DisplayedCamera;
+        internal int Rotation;
+        internal Image CameraFeed;
+    }
+}
