1f8f0a6021213a0207f60b700573e8c0278e93c8
Ethan <emad5n@umsystem.edu>
2020-10-04 19:39:57 -0500

    Finished implementing and fixing the parse and decodePacket functions for non-string variables

--- Electron/Core/RoveProtocol/Rovecomm.ts
+++ Electron/Core/RoveProtocol/Rovecomm.ts
@@ -4,6 +4,87 @@ const dgram = require("dgram")
 const net = require("net")
 const EventEmitter = require("events")
 
+function decodePacket(
+  size: number,
+  dataLength: number,
+  data: Uint8Array
+): number[] {
+  const retArray = []
+  let i = 0
+  let j = 0
+  switch (size) {
+    case 1:
+      while (i < dataLength) {
+        retArray.push(Number(data[i]))
+        i += 1
+      }
+      return retArray
+    case 2:
+      while (i < dataLength) {
+        // eslint-disable-next-line no-bitwise
+        const val = Number((data[j] << (1 * 8)) | data[j + 1])
+        retArray.push(val)
+        j += 2
+        i += 1
+      }
+      return retArray
+    case 4:
+      while (i < dataLength) {
+        const val = Number(
+          // eslint-disable-next-line no-bitwise
+          (data[j] << (3 * 8)) |
+            // eslint-disable-next-line no-bitwise
+            (data[j + 1] << (2 * 8)) |
+            // eslint-disable-next-line no-bitwise
+            (data[j + 2] << (1 * 8)) |
+            data[j + 3]
+        )
+        retArray.push(val)
+        i += 1
+        j += 4
+      }
+      return retArray
+    default:
+      return []
+  }
+}
+
+export function parse(packet: string): string {
+  // packets are currently packed as Version, DataIdHigh, DataIdLow, DataLength, DataType and then sizes[DataType] * DataLength bytes which contain the actual data
+  const VersionNumber = 2
+
+  enum DataTypes {
+    INT8_T = 0,
+    UINT8_T = 1,
+    INT16_T = 2,
+    UINT16_T = 3,
+    INT32_T = 4,
+    UINT32_T = 5,
+    FLOAT_T = 6,
+  }
+
+  const sizes = [1, 1, 2, 2, 4, 4, 4]
+
+  const version = Number(packet[0])
+  // eslint-disable-next-line no-bitwise
+  const dataId = Number((packet[1] << 8) | packet[2])
+  const dataLength = Number(packet[3])
+  const dataType = packet[4]
+
+  const rawdata = packet.slice(5)
+  let data: number[]
+
+  if (version === VersionNumber) {
+    console.log(dataId)
+    data = decodePacket(sizes[dataType], dataLength, new Uint8Array(rawdata))
+    // eslint-disable-next-line
+    emitHelper(DATAID[dataId], data)
+  } else {
+    return "null"
+  }
+  return DATAID[dataId]
+}
+
 function TCPListen(socket: any) {
   socket.on("data", (data: any) => {
     console.log(data, data.toString())
@@ -27,91 +108,18 @@ class Rovecomm extends EventEmitter {
   UDPListen() {
     this.UDPSocket.on(
       "message",
-      (msg: Buffer, rinfo: { address: string; port: number }) => {
+      (msg: string, rinfo: { address: string; port: number }) => {
         console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`)
-        this.parse(msg)
+        parse(msg)
       }
     )
-    this.UDPSocket.bind(8081)
+    this.UDPSocket.bind(11000)
   }
 }
 export const rovecomm = new Rovecomm()
 
-function decodePacket(
-  size: number,
-  dataLength: number,
-  data: Uint8Array
-): number[] {
-  const retArray = []
-  let i = 0
-  switch (size) {
-    case 1:
-      while (i < dataLength) {
-        retArray.push(Number(data[i]))
-        i += 1
-      }
-      return retArray
-    case 2:
-      while (i < dataLength) {
-        // eslint-disable-next-line no-bitwise
-        const val = Number(data[i] << (1 * 8) || data[i + 1])
-        retArray.push(val)
-        i += 2
-      }
-      return retArray
-    case 4:
-      while (i < dataLength) {
-        const val = Number(
-          // eslint-disable-next-line no-bitwise
-          data[i] << (3 * 8) ||
-            // eslint-disable-next-line no-bitwise
-            data[i + 1] << (2 * 8) ||
-            // eslint-disable-next-line no-bitwise
-            data[i + 2] << (1 * 8) ||
-            data[i + 3]
-        )
-        retArray.push(val)
-        i += 4
-      }
-      return retArray
-    default:
-      return []
-  }
-}
-
-export function parse(packet: Buffer): string {
-  // packets are currently packed as Version, DataIdHigh, DataIdLow, DataType, DataLength, and then sizes[DataType] * DataLength bytes which contain the actual data
-  const VersionNumber = 2
-
-  enum DataTypes {
-    INT8_T = 0,
-    UINT8_T = 1,
-    INT16_T = 2,
-    UINT16_T = 3,
-    INT32_T = 4,
-    UINT32_T = 5,
-    FLOAT_T = 6,
-  }
-
-  const sizes = [1, 1, 2, 2, 4, 4, 4]
-
-  const version = packet[0]
-  // eslint-disable-next-line no-bitwise
-  const dataId = (packet[1] << 8) | packet[2]
-  const dataType = packet[3]
-  const dataLength = packet[4]
-
-  const rawdata = packet.slice(5)
-  let data: number[]
-
-  if (version === VersionNumber) {
-    console.log(dataId)
-    data = decodePacket(sizes[dataType], dataLength, new Uint8Array(rawdata))
-    rovecomm.emit(DATAID[dataId], data)
-  } else {
-    return "null"
-  }
-  return DATAID[dataId]
+function emitHelper(dataID: string, data: any[]) {
+  rovecomm.emit(dataID, data)
 }
 
 export function sendCommand(packet: Packet, reliability = false) {
