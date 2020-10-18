50866d0e81b70f6819909b3dc9eb68f56668900a
rkbhkp <54925741+rkbhkp@users.noreply.github.com>
2020-10-18 16:46:21 -0500

    Made the file
    Made the file deleted some stuff from the gps which I copied for it.

---
+++ Electron/RED/components/ControlScheme.tsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react"
+import CSS from "csstype"
+import { rovecomm } from "../../Core/RoveProtocol/Rovecomm"
+// import { Packet } from "../../Core/RoveProtocol/Packet"
+
+const h1Style: CSS.Properties = {
+  fontFamily: "arial",
+  fontSize: "12px",
+}
+const container: CSS.Properties = {
+  display: "grid",
+  fontFamily: "arial",
+  width: "640px",
+  borderTopWidth: "28px",
+  borderColor: "#990000",
+  borderBottomWidth: "2px",
+  borderStyle: "solid",
+  gridRowStart: "2 & {}",
+  grid: "repeat(2, 28px) / auto-flow dense",
+}
+const label: CSS.Properties = {
+  marginTop: "-10px",
+  position: "relative",
+  top: "24px",
+  left: "3px",
+  fontFamily: "arial",
+  fontSize: "16px",
+  zIndex: 1,
+  color: "white",
+}
+
+interface IProps {}
+
+interface IState {}
+
+class ControlScheme extends Component<IProps, IState> {
+  constructor(props: any) {
+    super(props)
+    this.state = {}
+  }
+
+  render(): JSX.Element {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default GPS
