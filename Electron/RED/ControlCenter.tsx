01ce06930c6d4c436aeb8a998fc1ef93fee7207f
amwgvf <55326926+amwgvf@users.noreply.github.com>
2020-11-23 15:11:39 -0600

    adding RAM and RON buttons
    These buttons open RAM and RON, but there are still some issues with it that need to be fixed

--- Electron/RED/ControlCenter.tsx
+++ Electron/RED/ControlCenter.tsx
@@ -2,19 +2,70 @@ import React, { Component } from "react"
 import { rovecomm } from "../Core/RoveProtocol/Rovecomm"
 import GPS from "./components/GPS"
 import Log from "./components/Log"
+import NewWindowComponent from "./components/Window"
 
 interface IProps {}
 
-interface IState {}
+interface IState {
+  isNewWindow: boolean
+}
 
 class ControlCenter extends Component<IProps, IState> {
   constructor(props: any) {
     super(props)
-    this.state = {}
+    this.state = {
+      isNewWindow: false,
+    }
   }
 
   render(): JSX.Element {
     return (
+      <div>
+        {
+          // onClose will be fired when the new window is closed
+          // everything inside NewWindowComponent is considered props.children and will be
+          // displayed in a new window
+          this.state.isNewWindow === false && (
+            <NewWindowComponent>
+              onClose={() => this.setState({ isNewWindow: false })}
+              <h2>This will display in a new window</h2>
+            </NewWindowComponent>
+          )
+        }
+        <Log />
+        <GPS />
+        <button
+          type="button"
+          onClick={rovecomm.resubscribe}
+          style={{ width: "100px" }}
+        >
+          Resubscribe All
+        </button>
+        <button
+          type="button"
+          onClick={rovecomm.resubscribe}
+          style={{ width: "100px" }}
+        >
+          Resubscribe All
+        </button>
+        <button
+          type="button"
+          onClick={rovecomm.resubscribe}
+          style={{ width: "100px" }}
+        >
+          Resubscribe All
+        </button>
+        <button
+          type="button"
+          onClick={() => this.setState({ isNewWindow: true })}
+        >
+          Open in new window
+        </button>
+      </div>
+    )
+  }
+}
+/*
       <div>
         <Log />
         <GPS />
@@ -25,9 +76,20 @@ class ControlCenter extends Component<IProps, IState> {
         >
           Resubscribe All
         </button>
-      </div>
-    )
-  }
-}
+        <button
+          type="button"
+          onClick={rovecomm.resubscribe}
+          style={{ width: "100px" }}
+        >
+          Resubscribe All
+        </button>
+        <button
+          type="button"
+          onClick={rovecomm.resubscribe}
+          style={{ width: "100px" }}
+        >
+          Resubscribe All
+        </button>
+      </div> */
 
 export default ControlCenter
---
+++ Electron/RED/components/Window.tsx
@@ -0,0 +1,41 @@
+import { Component } from "react"
+import ReactDOM from "react-dom"
+
+interface IProps {}
+
+interface IState {
+  isNewWindow: false
+}
+
+class NewWindowComponent extends Component {
+  // Create a container <div> for the window
+  private containerEl = document.createElement("div")
+
+  // This will keep a reference of the window
+  private externalWindow:
+    | Window
+    | null
+    // When the component mounts, Open a new window
+    | undefined
+
+  // When the component mounts, Open a new window
+  componentDidMount() {
+    // The second argument in window.open is optional and can be set to whichever
+    // value you want. You will notice the use of this value when we modify the main
+    // electron.js file
+    this.externalWindow = window.open("", "NewWindowComponent ")
+
+    // Append the container div and register the event that will get fired when the
+    // window is closed
+    if (this.externalWindow) {
+      this.externalWindow.document.body.appendChild(this.containerEl)
+      // this.externalWindow.onunload = () => this.props.onClose()
+    }
+  }
+
+  render(): JSX.Element {
+    return ReactDOM.createPortal(this.props.children, this.containerEl)
+  }
+}
+
+export default NewWindowComponent
