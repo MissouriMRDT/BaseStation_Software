151e1f295c99c2c03b48aa4494484e0b1c373fe4
rkbhkp <rkbhkp@users.noreply.github.com>
2020-11-24 00:24:37 -0600

    Fixed the toggle of the buttons
    the buttons for the controler scheme now toggle on and off, but there is a green highlight

--- Electron/RED/components/ControlScheme.tsx
+++ Electron/RED/components/ControlScheme.tsx
@@ -8,7 +8,7 @@ const h1Style: CSS.Properties = {
   fontSize: "12px",
 }
 const container: CSS.Properties = {
-  display: "grid",
+  display: "block",
   fontFamily: "arial",
   width: "640px",
   borderTopWidth: "28px",
@@ -29,12 +29,37 @@ const label: CSS.Properties = {
   grid: "repeat(2, 28px) /auto-flow dense",
 }
 
+const roAndBtnContainer: CSS.Properties = {
+  // stands for "Readout and Button Container"; shortened for sanity
+  display: "grid",
+  width: "auto",
+  gridTemplateColumns: "69px 1fr 69px 1fr",
+  marginLeft: "2px",
+  marginTop: "2px",
+  marginBottom: "2px",
+}
+
+const readoutDisplay: CSS.Properties = {
+  display: "grid",
+  gridTemplateColumns: "auto auto",
+  fontSize: "12px",
+  backgroundColor: "#30ff00", // green
+  justifyContent: "space-between",
+  fontFamily: "arial",
+  paddingTop: "4px",
+  paddingLeft: "3px",
+  paddingRight: "3px",
+  paddingBottom: "4px",
+  marginRight: "2px",
+}
+
 interface IProps {
-  defaultState: number
+  defaultState: false
 }
 
 interface IState {
-  currentState: number
+  currentState: boolean
+  motorLF: number
 }
 
 class ControlScheme extends Component<IProps, IState> {
@@ -42,6 +67,7 @@ class ControlScheme extends Component<IProps, IState> {
     super(props)
     this.state = {
       currentState: this.props.defaultState,
+      motorLF: 0.0,
     }
   }
 
@@ -50,27 +76,51 @@ class ControlScheme extends Component<IProps, IState> {
       <div>
         <div style={label}>ControlScheme</div>
         <div style={container}>
-          {[
-            { title: "Drive", value: "on" },
-            { title: "Main Gimbal", value: "off" },
-          ].map(datum => {
-            const { title, value } = datum
-            return (
-              <>
-                <div>{title}</div>
-                <button
-                  type="button"
-                  key={value}
-                  onClick={() => this.setState({ currentState: value })}
-                >
-                  <h1 style={h1Style}>{value}</h1>
-                </button>
-              </>
-            )
-          })}
+          <div style={readoutDisplay}>
+            <div style={readoutDisplay}>
+                <span>Drive</span>
+            </div>
+            <ToggleButton>
+                {({ on, toggle }) => (
+                  <button type="button" onClick={toggle}>
+                    {on ? "On" : "Off"}
+                  </button>
+                )}
+            </ToggleButton>
+            <div style={readoutDisplay}>
+                <span>Main Gimbal</span>
+            </div>
+            <ToggleButton>
+                {({ on, toggle }) => (
+                  <button type="button" onClick={toggle}>
+                    {on ? "On" : "Off"}
+                  </button>
+                )}
+            </ToggleButton>
+          </div>
         </div>
       </div>
     )
+  }
+}
+
+class ToggleButton extends ControlScheme {
+  state = {
+    on: false,
+  }
+
+  toggle = () => {
+    this.setState({
+      on: !this.state.on,
+    })
+  }
+
+  render() {
+    const { children } = this.props
+    return children({
+      on: this.state.on,
+      toggle: this.toggle,
+    })
   }
 }
 
