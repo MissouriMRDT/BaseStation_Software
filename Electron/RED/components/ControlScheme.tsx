8b022f8bb25f63eae092683341953929f3de96a0
rkbhkp <rkbhkp@users.noreply.github.com>
2020-12-03 00:33:59 -0600

    Revert "Temporary Submition, working on adding controller functionality"
    This reverts commit 9a605490afdc89d6d2c64187009df72980283f53.

--- Electron/RED/components/ControlScheme.tsx
+++ Electron/RED/components/ControlScheme.tsx
@@ -1,11 +1,6 @@
-import React, { Component, ReactNode, useState } from "react"
-import { render } from "react-dom";
+import React, { Component } from "react"
 import CSS from "csstype"
-import { useGamepads } from 'react-gamepads'
-import GamepadController from "./GamepadController"
-
-
-//import { rovecomm } from "../../Core/RoveProtocol/Rovecomm"
+import { rovecomm } from "../../Core/RoveProtocol/Rovecomm"
 // import { Packet } from "../../Core/RoveProtocol/Packet"
 
 const h1Style: CSS.Properties = {
@@ -13,15 +8,13 @@ const h1Style: CSS.Properties = {
   fontSize: "12px",
 }
 const container: CSS.Properties = {
-  display: "grid",
+  display: "block",
   fontFamily: "arial",
   width: "640px",
   borderTopWidth: "28px",
   borderColor: "#990000",
   borderBottomWidth: "2px",
   borderStyle: "solid",
-  grid: "repeat(1, 28px) / auto-flow dense",
-
 }
 const label: CSS.Properties = {
   marginTop: "-10px",
@@ -36,10 +29,21 @@ const label: CSS.Properties = {
   grid: "repeat(2, 28px) /auto-flow dense",
 }
 
+const roAndBtnContainer: CSS.Properties = {
+  // stands for "Readout and Button Container"; shortened for sanity
+  display: "grid",
+  width: "auto",
+  gridTemplateColumns: "69px 1fr 69px 1fr",
+  marginLeft: "2px",
+  marginTop: "2px",
+  marginBottom: "2px",
+}
+
 const readoutDisplay: CSS.Properties = {
   display: "grid",
   gridTemplateColumns: "auto auto",
-  fontSize: "14px",
+  fontSize: "12px",
+  backgroundColor: "#30ff00", // green
   justifyContent: "space-between",
   fontFamily: "arial",
   paddingTop: "4px",
@@ -47,50 +51,23 @@ const readoutDisplay: CSS.Properties = {
   paddingRight: "3px",
   paddingBottom: "4px",
   marginRight: "2px",
-  grid: "repeat(2, 28px) /auto-flow dense",
-  }
-
-interface IProps {
-  children: ReactNode,
 }
 
-interface IState {}
+interface IProps {
+  defaultState: false
+}
 
-export const Switch = () => {
-  const [open, toggle] = useToggle();
-  const [open2, toggle2] = useToggle();
-  return (
-    <>
-      <p>Testing toggle 1: {`${open}`}</p>
-      <p>Testing toggle 2: {`${open2}`}</p>
-      <button
-        onClick={() => {
-          toggle();
-        }}
-      >
-        Toggle 1
-      </button>
-      <button
-        onClick={() => {
-          toggle2();
-        }}
-      >
-        Toggle 2
-      </button>
-    </>
-  );
-};
-
-export const useToggle = (initialMode = false) => {
-  const [open, setOpen] = useState(initialMode);
-  const toggle = () => setOpen(!open);
-  return [open, toggle, setOpen] as const;
-};
+interface IState {
+  currentState: boolean
+  motorLF: number
+}
 
 class ControlScheme extends Component<IProps, IState> {
   constructor(props: any) {
     super(props)
     this.state = {
+      currentState: this.props.defaultState,
+      motorLF: 0.0,
     }
   }
 
@@ -99,43 +76,28 @@ class ControlScheme extends Component<IProps, IState> {
       <div>
         <div style={label}>ControlScheme</div>
         <div style={container}>
-          <switch />
           <div style={readoutDisplay}>
-              Drive
+            <div style={readoutDisplay}>
+                <span>Drive</span>
+            </div>
+            <ToggleButton>
+                {({ on, toggle }) => (
+                  <button type="button" onClick={toggle}>
+                    {on ? "On" : "Off"}
+                  </button>
+                )}
+            </ToggleButton>
+            <div style={readoutDisplay}>
+                <span>Main Gimbal</span>
+            </div>
+            <ToggleButton>
+                {({ on, toggle }) => (
+                  <button type="button" onClick={toggle}>
+                    {on ? "On" : "Off"}
+                  </button>
+                )}
+            </ToggleButton>
           </div>
-          <select>
-            <option selected value="Xbox 1">Xbox 1</option>
-            <option value="Xbox 2">Xbox 2</option>
-          </select>
-          <select>
-            <option selected value="Tank Drive">Tank Drive</option>
-            <option value="Xbox Gimbal">Xbox Gimbal</option>
-          </select>
-          <ToggleButton>
-            {({ on, toggle }) => (
-              <button type="button" onClick={toggle}>
-                {on ? "On" : "Off"}
-              </button>
-          )}
-          </ToggleButton>
-          <div style={readoutDisplay}>
-              Main Gimbal
-          </div>
-          <select>
-            <option value="Xbox 1">Xbox 1</option>
-            <option selected value="Xbox 2">Xbox 2</option>
-          </select>
-          <select>
-            <option value="Tank Drive">Tank Drive</option>
-            <option selected value="Xbox Gimbal">Xbox Gimbal</option>
-          </select>
-          <ToggleButton>
-              {({ on, toggle }) => (
-                <button type="button" onClick={toggle}>
-                  {on ? "On" : "Off"}
-                </button>
-              )}
-          </ToggleButton>
         </div>
       </div>
     )
@@ -163,4 +125,3 @@ class ToggleButton extends ControlScheme {
 }
 
 export default ControlScheme
-
