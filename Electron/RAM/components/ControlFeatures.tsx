17738568a1a2846f1128ebb9fe2687e41d80762e
thomas-gleiforst <ps2dude67@gmail.com>
2021-01-15 10:45:15 -0600

    beginnings of controlFeatures component

---
+++ Electron/RAM/components/ControlFeatures.tsx
@@ -0,0 +1,83 @@
+import React, { Component } from "react"
+import CSS from "csstype"
+import { rovecomm } from "../../Core/RoveProtocol/Rovecomm"
+
+const header: CSS.Properties = {
+  fontFamily: "arial",
+  fontSize: "16px",
+  lineHeight: "22px",
+  width: "35%",
+}
+const value: CSS.Properties = {
+  fontFamily: "arial",
+  fontSize: "16px",
+  lineHeight: "22px",
+  width: "10%",
+  textAlign: "center",
+}
+const container: CSS.Properties = {
+  display: "flex",
+  flexDirection: "column",
+  fontFamily: "arial",
+  width: "640px",
+  borderTopWidth: "28px",
+  borderColor: "#990000",
+  borderBottomWidth: "2px",
+  borderStyle: "solid",
+}
+const label: CSS.Properties = {
+  marginTop: "-10px",
+  position: "relative",
+  top: "24px",
+  left: "3px",
+  fontFamily: "arial",
+  fontSize: "16px",
+  zIndex: 1,
+  color: "white",
+}
+const row: CSS.Properties = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-around",
+  alignItems: "center",
+  margin: "5px",
+}
+
+interface IProps {}
+
+interface IState {
+  overridden: boolean
+}
+
+class ControlFeatures extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props)
+    this.state = {
+      overridden: false,
+    }
+  }
+
+  limitOverride() {
+    // we send 1 when false and 0 when true because we are about to toggle the bool
+    rovecomm.sendCommand("LimitSwitchOverride", this.state.overridden ? 0 : 1)
+    this.setState({ overridden: !this.state.overridden })
+  }
+
+  render(): JSX.Element {
+    return (
+      <div>
+        <div style={label}>Control Multipliers</div>
+        <div style={container}>
+          <div style={row}>
+            <div style={header}>Limit Override: </div>
+            <button type="button" onClick={this.limitOverride}>
+              All Joints
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ControlFeatures
