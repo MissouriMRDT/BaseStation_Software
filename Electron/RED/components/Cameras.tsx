7722013213697c26c1b41510f2f63a6cd32a20f6
thomas-gleiforst <ps2dude67@gmail.com>
2021-01-21 21:51:51 -0600

    Set up support for autonomy streams

--- Electron/RED/components/Cameras.tsx
+++ Electron/RED/components/Cameras.tsx
@@ -8,13 +8,15 @@ const h1Style: CSS.Properties = {
   fontSize: "12px",
 }
 const container: CSS.Properties = {
-  display: "grid",
+  display: "flex",
+  flexDirection: "column",
   fontFamily: "arial",
   width: "640px",
   borderTopWidth: "28px",
   borderColor: "#990000",
   borderBottomWidth: "2px",
   borderStyle: "solid",
+  padding: "5px",
 }
 const label: CSS.Properties = {
   marginTop: "-10px",
@@ -45,7 +47,12 @@ class Cameras extends Component<IProps, IState> {
     super(props)
     this.state = {
       currentCamera: this.props.defaultCamera,
-      baseAddress: ["http://192.168.1.50:8080", "http://192.168.1.51:8080"],
+      baseAddress: [
+        "http://192.168.1.50:8080",
+        "http://192.168.1.51:8080",
+        "http://192.168.1.139:8081",
+        "http://192.168.1.139:8082",
+      ],
     }
 
     // rovecomm.sendCommand(Packet(dataId, data), reliability)
@@ -55,6 +62,12 @@ class Cameras extends Component<IProps, IState> {
     const index = Math.floor((this.state.currentCamera - 1) / 4)
     const camera = ((this.state.currentCamera - 1) % 4) + 1
     const addr = this.state.baseAddress[index]
+    if (this.state.currentCamera === 9) {
+      return this.state.baseAddress[2]
+    }
+    if (this.state.currentCamera === 10) {
+      return this.state.baseAddress[3]
+    }
     return `${addr}/${camera}/stream`
   }
 
@@ -64,23 +77,23 @@ class Cameras extends Component<IProps, IState> {
         <div style={label}>Cameras</div>
         <div style={container}>
           <div style={row}>
-            {[1, 2, 3, 4, 5, 6, 7, 8].map(num => {
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(num => {
               return (
                 <button
                   type="button"
                   key={num}
                   onClick={() => this.setState({ currentCamera: num })}
+                  style={{ flexGrow: 1 }}
                 >
                   <h1 style={h1Style}>{num}</h1>
                 </button>
               )
             })}
           </div>
-          <iframe
-            title="test"
+          <img
             src={this.ConstructAddress()}
-            width="640"
-            height="500"
+            alt={`Camera ${this.state.currentCamera}`}
+            style={{ flexGrow: 1 }}
           />
         </div>
       </div>
