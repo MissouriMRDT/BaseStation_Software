e438b8c7933e68171df954294d4f57f2ee3d2e7a
amwgvf <55326926+amwgvf@users.noreply.github.com>
2021-02-09 18:10:34 -0600

    creating a function for arm
    this function sets the values to send to rovecomm for arm

--- Electron/src/RAM/Arm/Arm.tsx
+++ Electron/src/RAM/Arm/Arm.tsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react"
 import CSS from "csstype"
-import ControlMultipliers from "./components/ControlMultipliers"
+import ControlMultipliers, { controlMultipliers } from "./components/ControlMultipliers"
 import IK from "./components/IK"
 import Angular from "./components/Angular"
 import Cameras from "../../Core/components/Cameras"
+import { controllerInputs } from "../../Core/components/ControlScheme"
+import { rovecomm } from "../../Core/RoveProtocol/Rovecomm"
 
 const row: CSS.Properties = {
   display: "flex",
@@ -18,6 +20,57 @@ const column: CSS.Properties = {
   marginRight: "5px",
 }
 
+function arm(): void {
+  let ArmWristBend = 0
+  let ArmWristTwist = 0
+  let ArmElbowTwist = 0
+  let ArmElbowBend = 0
+  let ArmBaseTwist = 0
+  let ArmBaseBend = 0
+  let Gripper = 0
+  let Nipper = 0
+
+  if ("WristBend" in controllerInputs && "WristTwist" in controllerInputs) {
+    ArmWristBend = controllerInputs.WristBend * controlMultipliers.Wrist
+    ArmWristTwist = controllerInputs.WristTwist * controlMultipliers.Wrist
+  }
+
+  if ("ElbowBend" in controllerInputs && "ElbowTwist" in controllerInputs) {
+    ArmElbowBend = controllerInputs.ElbowBend * controlMultipliers.Elbow
+    ArmElbowTwist = controllerInputs.ElbowTwist * controlMultipliers.Elbow
+  }
+
+  if ("BaseBendDirection" in controllerInputs && "BaseBendMagnitude" in controllerInputs) {
+    const direction = controllerInputs.BaseBendDirection === 1 ? -1 : 1
+    ArmBaseBend = direction * controllerInputs.BaseBendMagnitude * controlMultipliers.Base
+  }
+
+  if ("BaseTwistDirection" in controllerInputs && "BaseTwistMagnitude" in controllerInputs) {
+    const direction = controllerInputs.BaseTwistDirection === 1 ? -1 : 1
+    ArmBaseTwist = direction * controllerInputs.BaseTwistMagnitude * controlMultipliers.Base
+  }
+
+  if ("GripperOpen" in controllerInputs && "GripperClose" in controllerInputs) {
+    if (controllerInputs.GripperOpen === 1) {
+      Gripper = 1 * controlMultipliers.Gripper
+    } else if (controllerInputs.GripperClose === 1) {
+      Gripper = -1 * controlMultipliers.Gripper
+    } else {
+      Gripper = 0
+    }
+  }
+
+  if ("Nipper" in controllerInputs) {
+    Nipper = controllerInputs.Nipper
+  }
+
+  const armValues = [ArmWristBend, ArmWristTwist, ArmElbowTwist, ArmElbowBend, ArmBaseTwist, ArmBaseBend]
+
+  rovecomm.sendCommand("ArmVelocityControl", armValues)
+  rovecomm.sendCommand("GripperMove", Gripper)
+  // rovecomm.sendCommand("NipperMove", Nipper)
+}
+
 interface IProps {}
 
 interface IState {}
@@ -26,6 +79,8 @@ class Arm extends Component<IProps, IState> {
   constructor(props: IProps) {
     super(props)
     this.state = {}
+
+    setInterval(() => arm(), 100)
   }
 
   render(): JSX.Element {
