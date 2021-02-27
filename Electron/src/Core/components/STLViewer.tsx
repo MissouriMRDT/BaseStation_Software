3b4fc3ef76f5ca62192931c75a25e56846d3f73d
thomas-gleiforst <ps2dude67@gmail.com>
2021-02-26 23:23:40 -0600

    Migrate stlviewer to tsx & update deprecated

--- Electron/src/Core/components/STLViewer.tsx
+++ Electron/src/Core/components/STLViewer.tsx
@@ -3,27 +3,29 @@ import PropTypes from "prop-types"
 import { ScaleLoader } from "halogenium"
 import Paint from "./Paint"
 
-class STLViewer extends Component {
-  static propTypes = {
-    className: PropTypes.string,
-    url: PropTypes.string,
-    width: PropTypes.number,
-    height: PropTypes.number,
-    backgroundColor: PropTypes.string,
-    modelColor: PropTypes.string,
-    rotate: PropTypes.bool,
-    orbitControls: PropTypes.bool,
-    cameraX: PropTypes.number,
-    cameraY: PropTypes.number,
-    cameraZ: PropTypes.number,
-    lights: PropTypes.array,
-    lightColor: PropTypes.string,
-    rotation: PropTypes.arrayOf(PropTypes.number),
-    rotationSpeeds: PropTypes.arrayOf(PropTypes.number),
-    model: PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(ArrayBuffer)]).isRequired,
-    zoom: PropTypes.number,
-  }
+interface IProps {
+  className?: string
+  url?: string
+  width?: number
+  height?: number
+  backgroundColor?: string
+  modelColor?: string
+  rotate?: boolean
+  orbitControls?: boolean
+  cameraX?: number
+  cameraY?: number
+  cameraZ?: number
+  lights?: any
+  lightColor?: string
+  rotation?: number[]
+  rotationSpeeds?: number[]
+  model: any
+  zoom?: number
+}
 
+interface IState {}
+
+class STLViewer extends Component<IProps, IState> {
   static defaultProps = {
     backgroundColor: "#EAEAEA",
     modelColor: "#B92C2C",
@@ -42,23 +44,18 @@ class STLViewer extends Component {
     zoom: 30,
   }
 
+  paint = new Paint()
+
   componentDidMount() {
-    this.paint = new Paint()
     this.paint.init(this)
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return JSON.stringify(nextProps) !== JSON.stringify(this.props)
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    this.props = nextProps
+  componentDidUpdate() {
     this.paint.init(this)
   }
 
   componentWillUnmount() {
     this.paint.clean()
-    delete this.paint
   }
 
   render() {
