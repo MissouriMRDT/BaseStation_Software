ce693db8c45b6bce77e1816b2862003ed82947a9
ThisIsBrady <30638248+ThisIsBrady@users.noreply.github.com>
2021-11-09 18:30:24 -0600

    Start layout for Science Hardware control

--- Electron/src/RAM/Science/components/ScienceHardware.tsx
+++ Electron/src/RAM/Science/components/ScienceHardware.tsx
@@ -25,38 +25,26 @@ const label: CSS.Properties = {
 const row: CSS.Properties = {
   display: "flex",
   flexDirection: "row",
-  justifyContent: "space-around",
+  justifyContent: "center",
   marginTop: "5px",
   lineHeight: "25px",
-}
-const immutable: CSS.Properties = {
-  backgroundColor: "#F0F0F0",
-  width: "25px",
-  fontSize: "18px",
-  textAlign: "center",
 }
 const buttonRow: CSS.Properties = {
   display: "flex",
   flexDirection: "row",
-  justifyContent: "space-around",
+  justifyContent: "center",
   marginTop: "5px",
   marginBottom: "5px",
 }
-const button: CSS.Properties = {
-  width: "60px",
-  height: "60px",
-  borderRadius: "10px",
-  boxShadow: "3px 3px 3px gray",
-  border: "none",
-  outline: "none",
+const leftGroup: CSS.Properties = {
+  width: "50%",
+  justifyContent: "center",
+  margin: "auto",
 }
-const disabledButton: CSS.Properties = {
-  width: "60px",
-  height: "60px",
-  borderRadius: "10px",
-  boxShadow: "3px 3px 3px gray inset",
-  border: "none",
-  outline: "none",
+const rightGroup: CSS.Properties = {
+  width: "40%",
+  justifyContent: "center",
+  margin: "auto",
 }
 
 const testTube = 12
@@ -116,31 +104,34 @@ class Geneva extends Component<IProps, IState> {
   render(): JSX.Element {
     return (
       <div style={this.props.style}>
-        <div style={label}>Geneva Position</div>
+        <div style={label}>Science Hardware</div>
         <div style={container}>
           <div style={row}>
-            <div>Test Tube:</div>
-            <div style={immutable}>{this.state.tube}</div>
-          </div>
-          <div style={buttonRow}>
-            <button
-              type="button"
-              style={this.state.buttonsDisabled ? disabledButton : button}
-              onClick={this.rotateLeft}
-            >
-              Rotate
-              <br />
-              Left
-            </button>
-            <button
-              type="button"
-              style={this.state.buttonsDisabled ? disabledButton : button}
-              onClick={this.rotateRight}
-            >
-              Rotate
-              <br />
-              Right
-            </button>
+            <div style={leftGroup}>
+              <div style={row}>Move Gantry to Test Tubes</div>
+              <div style={buttonRow}>
+                <button type="button">Grp 1</button>
+                <button type="button">Grp 2</button>
+                <button type="button">Grp 3</button>
+              </div>
+              <div style={row}>Move to Spare Scoop</div>
+              <div style={buttonRow}>
+                <button type="button">Test 1</button>
+                <button type="button">Test 2</button>
+                <button type="button">Test 2</button>
+              </div>
+            </div>
+            <div style={rightGroup}>
+              <div style={row}>Testing</div>
+              <div style={row}>
+                <button type="button">Grp 1</button>
+                <button type="button">Grp 2</button>
+              </div>
+              <div style={row}>
+                <button type="button">Grp 3</button>
+                <button type="button">Drill</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
