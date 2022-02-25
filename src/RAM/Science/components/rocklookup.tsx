736246e5b81d4fe8a8e83248cd58b17526748540
ThisIsBrady <bradyh2001@gmail.com>
2022-02-25 01:11:03 -0600

    Progress on rocklookup. GUI almost done. Needs polishing, but should be in working state.

--- src/RAM/Science/components/rocklookup.tsx
+++ src/RAM/Science/components/rocklookup.tsx
@@ -2,7 +2,6 @@ import React, { Component } from "react"
 import CSS from "csstype"
 import path from "path"
 import fs from "fs"
-import { group } from "console"
 
 const label: CSS.Properties = {
   color: "white",
@@ -59,10 +58,22 @@ const tagList: CSS.Properties = {
 }
 
 const tagStyle: CSS.Properties = {
-  border: "1px black",
+  display: "flex",
+  flexDirection: "row",
+  border: "1px solid black",
+  margin: "5px",
+  lineHeight: ".1",
+  padding: "0 5px 0 5px",
+}
+
+enum FeatureType {
+  COLOR,
+  CLEAVE,
+  FORM,
 }
 
 const ROCKPATH = path.join(__dirname, "../assets/rockLookupAssets/RockDatabase.tsv")
+
 /**
  * Read the rock definitions from the tsv file
  */
@@ -126,8 +137,11 @@ function populateColors(): string[] {
       colorList.push(color)
     })
   })
+  colorList.sort()
   return [...new Set(colorList)]
 }
+
+const COLORMASTER: string[] = populateColors()
 
 function populateForms(): string[] {
   let formList: string[] = []
@@ -136,8 +150,11 @@ function populateForms(): string[] {
       formList.push(form)
     })
   })
+  formList.sort()
   return [...new Set(formList)]
 }
+
+const FORMMASTER: string[] = populateForms()
 
 function populateCleaves(): string[] {
   let cleaveList: string[] = []
@@ -146,8 +163,11 @@ function populateCleaves(): string[] {
       cleaveList.push(cleave)
     })
   })
+  cleaveList.sort()
   return [...new Set(cleaveList)]
 }
+
+const CLEAVEMASTER: string[] = populateCleaves()
 
 interface Minerals {
   name: string
@@ -166,6 +186,16 @@ interface Rocks {
 interface Output {
   Rock: Rocks
   ConfidenceScore: number
+}
+
+var outputCompare = (out1: Output, out2: Output) => {
+  if (out1.ConfidenceScore > out2.ConfidenceScore) {
+    return -1
+  }
+  if (out1.ConfidenceScore < out2.ConfidenceScore) {
+    return 1
+  }
+  return 0
 }
 
 interface IProps {
@@ -191,11 +221,11 @@ class RockLookUp extends Component<IProps, IState> {
   constructor(props: Readonly<IProps>) {
     super(props)
     this.state = {
-      availColors: populateColors(),
+      availColors: JSON.parse(JSON.stringify(COLORMASTER)),
       s_Colors: [],
-      availForms: populateForms(),
+      availForms: JSON.parse(JSON.stringify(FORMMASTER)),
       s_Forms: [],
-      availCleave: populateCleaves(),
+      availCleave: JSON.parse(JSON.stringify(CLEAVEMASTER)),
       s_Cleave: [],
       searchField: "WIP; Feature frozen",
       outputArr: [],
@@ -204,51 +234,125 @@ class RockLookUp extends Component<IProps, IState> {
     this.handleFeatureSubmit = this.handleFeatureSubmit.bind(this)
     this.handleInputChange = this.handleInputChange.bind(this)
     this.handleFeatureSelect = this.handleFeatureSelect.bind(this)
+    this.compareSelections = this.compareSelections.bind(this)
   }
-/*
+
+  reloadOptions(): void {
+    this.setState(
+      {
+        availCleave: JSON.parse(JSON.stringify(CLEAVEMASTER)),
+        availForms: JSON.parse(JSON.stringify(FORMMASTER)),
+        availColors: JSON.parse(JSON.stringify(COLORMASTER)),
+      },
+      () => this.compareSelections()
+    )
+  }
+
   compareSelections() {
-    let { availColors, s_Colors, availForms, s_Forms, availCleave, s_Cleave } = this.state
-    let numHits = 0
-    let selectedMins = []
-    MINARR.map(mineral => {
-      s_Colors.map(color => {
-        if (mineral.colors.indexOf(color) !== -1) {
-          numHits++
+    let { s_Colors, s_Forms, s_Cleave } = this.state
+    let possRock: Output[] = []
+    let selectedMins: Minerals[] = []
+    if (s_Colors.length || s_Forms.length || s_Cleave.length) {
+      MINARR.forEach(mineral => {
+        let hit: boolean = false
+        s_Colors.forEach(color => {
+          if (mineral.colors.indexOf(color)) {
+            hit = true
+          }
+        })
+        s_Forms.forEach(form => {
+          if (mineral.forms.indexOf(form)) {
+            hit = true
+          }
+        })
+        s_Cleave.forEach(cleave => {
+          if (mineral.cleaveAndLuster.indexOf(cleave)) {
+            hit = true
+          }
+        })
+        if (hit) {
+          selectedMins.push(mineral)
         }
       })
-      s_Forms.map(form => {
-        if (mineral.forms.indexOf(form) !== -1) {
-          numHits++
+      selectedMins = [...new Set(selectedMins)]
+      this.cullImpossibles(selectedMins)
+    }
+    ROCKARR.forEach(rock => {
+      let confScore: number = 0
+      selectedMins.forEach(mineral => {
+        if (rock.ComMinerals.indexOf(mineral.name) >= 0) {
+          confScore += 1
+        }
+        if (rock.ReqMinerals.indexOf(mineral.name) >= 0) {
+          confScore += 2
         }
       })
-      s_Cleave.map(cleave => {
-        if (mineral.cleaveAndLuster.indexOf(cleave) !== -1) {
-          numHits++
-        }
-      })
-      if (numHits) {
+      if (confScore > 0) {
+        possRock.push({ Rock: rock, ConfidenceScore: confScore })
       }
     })
+    possRock = [...new Set(possRock)]
+    possRock.sort(outputCompare)
+    this.setState({ outputArr: possRock })
   }
-*/
+
+  cullImpossibles(possibleMins: Minerals[]): void {
+    let { availCleave, availColors, availForms } = this.state
+
+    availCleave.forEach(cleave => {
+      let isPoss: number = 0
+      possibleMins.forEach(mineral => {
+        if (mineral.cleaveAndLuster.indexOf(cleave)) {
+          isPoss++
+        }
+      })
+      if (isPoss === 0) {
+        availCleave.splice(availCleave.indexOf(cleave), 1)
+      }
+    })
+    availColors.forEach(color => {
+      let isPoss: number = 0
+      possibleMins.forEach(mineral => {
+        if (mineral.colors.indexOf(color)) {
+          isPoss++
+        }
+      })
+      if (isPoss === 0) {
+        availColors.splice(availColors.indexOf(color), 1)
+      }
+    })
+    availForms.forEach(form => {
+      let isPoss: number = 0
+      possibleMins.forEach(mineral => {
+        if (mineral.forms.indexOf(form)) {
+          isPoss++
+        }
+      })
+      if (isPoss === 0) {
+        availForms.splice(availForms.indexOf(form), 1)
+      }
+    })
+    this.setState({ availCleave, availColors, availForms })
+  }
+
   handleFeatureSelect(event: any): void {
     if (event.target.id === "ColorList") {
       let { s_Colors, availColors } = this.state
       availColors.splice(availColors.indexOf(event.target.value), 1)
       s_Colors.push(event.target.value)
-      this.setState({ s_Colors, availColors })
+      this.setState({ s_Colors, availColors }, () => this.compareSelections())
     }
     if (event.target.id === "CleaveList") {
       let { s_Cleave, availCleave } = this.state
       availCleave.splice(availCleave.indexOf(event.target.value), 1)
       s_Cleave.push(event.target.value)
-      this.setState({ s_Cleave, availCleave })
+      this.setState({ s_Cleave, availCleave }, () => this.compareSelections())
     }
     if (event.target.id === "FormList") {
       let { s_Forms, availForms } = this.state
       availForms.splice(availForms.indexOf(event.target.value), 1)
       s_Forms.push(event.target.value)
-      this.setState({ s_Forms, availForms })
+      this.setState({ s_Forms, availForms }, () => this.compareSelections())
     }
   }
 
@@ -261,20 +365,62 @@ class RockLookUp extends Component<IProps, IState> {
     this.setState({ searchField: event.target.value })
   }
 
+  removeSelectedFeature(type: FeatureType, index: number): void {
+    switch (type) {
+      case FeatureType.COLOR:
+        let { availColors, s_Colors } = this.state
+        availColors.push(s_Colors[index])
+        availColors.sort()
+        s_Colors.splice(index, 1)
+        this.setState({ s_Colors, availColors }, () => this.reloadOptions())
+        break
+      case FeatureType.CLEAVE:
+        let { availCleave, s_Cleave } = this.state
+        availCleave.push(s_Cleave[index])
+        availCleave.sort()
+        s_Cleave.splice(index, 1)
+        this.setState({ s_Cleave, availCleave }, () => this.reloadOptions())
+        break
+      case FeatureType.FORM:
+        let { availForms, s_Forms } = this.state
+        availForms.push(s_Forms[index])
+        availForms.sort()
+        s_Forms.splice(index, 1)
+        this.setState({ s_Forms, availForms }, () => this.reloadOptions())
+        break
+    }
+  }
+
   clearSelectedFeatures(): void {
-    let { availColors, availCleave, availForms } = this.state
-    let { s_Colors, s_Cleave, s_Forms } = this.state
-    availColors.push.apply(availColors, s_Colors)
-    availCleave.push.apply(availCleave, s_Cleave)
-    availForms.push.apply(availForms, s_Forms)
-    this.setState({
-      s_Colors: [],
-      s_Cleave: [],
-      s_Forms: [],
-      availColors,
-      availCleave,
-      availForms,
-    })
+    this.setState(
+      {
+        s_Colors: [],
+        s_Cleave: [],
+        s_Forms: [],
+      },
+      () => this.reloadOptions()
+    )
+  }
+
+  possibleRocks(): JSX.Element | void {
+    if (this.state.outputArr.length > 0) {
+      console.log("here dood")
+      return (
+        <div>
+          {this.state.outputArr[this.state.selectedOutput].Rock.name}:
+          {this.state.outputArr[this.state.selectedOutput].Rock.description}
+        </div>
+      )
+    }
+    /*this.state.selectedOutput > 0 && this.state.outputArr.length > 0
+      ? () => {
+          return (
+            <button type={"button"} onClick={() => this.setState({ selectedOutput: this.state.selectedOutput - 1 })}>
+              ^^^
+            </button>
+          )
+        }
+      : null*/
   }
 
   render(): JSX.Element {
@@ -282,14 +428,8 @@ class RockLookUp extends Component<IProps, IState> {
       <div style={this.props.style}>
         <div style={label}>Rock Lookup</div>
         <div style={container}>
-          <div style={row}>
-            <form
-              onSubmit={
-                () => {
-                  this.handleFeatureSubmit
-                } /* TODO */
-              }
-            >
+          {/*<div style={row}>
+            <form onSubmit={e => this.handleFeatureSubmit(e)}>
               <input
                 type="text"
                 name="newTag"
@@ -299,7 +439,7 @@ class RockLookUp extends Component<IProps, IState> {
               />
               <input type="submit" value={this.state.searchField} />
             </form>
-          </div>
+              </div>*/}
           <div style={row}>
             <div style={featureColumn}>
               <p>Colors</p>
@@ -346,17 +486,79 @@ class RockLookUp extends Component<IProps, IState> {
           </div>
           <div style={column}>
             <div style={tagList}>
-              {[...this.state.s_Colors, ...this.state.s_Forms, ...this.state.s_Cleave].map((tag: string) => {
+              {this.state.s_Colors.map((tag: string, index: number) => {
                 return (
                   <div style={tagStyle}>
                     <p>{tag}</p>
-                    <p onClick={() => {} /* TODO */}>x</p>
+                    <p
+                      onClick={() => this.removeSelectedFeature(FeatureType.COLOR, index)}
+                      style={{ marginLeft: "10px", color: "red" }}
+                    >
+                      x
+                    </p>
+                  </div>
+                )
+              })}
+              {this.state.s_Cleave.map((tag: string, index: number) => {
+                return (
+                  <div style={tagStyle}>
+                    <p>{tag}</p>
+                    <p
+                      onClick={() => this.removeSelectedFeature(FeatureType.CLEAVE, index)}
+                      style={{ marginLeft: "10px", color: "red" }}
+                    >
+                      x
+                    </p>
+                  </div>
+                )
+              })}
+              {this.state.s_Forms.map((tag: string, index: number) => {
+                return (
+                  <div style={tagStyle}>
+                    <p>{tag}</p>
+                    <p
+                      onClick={() => this.removeSelectedFeature(FeatureType.FORM, index)}
+                      style={{ marginLeft: "10px", color: "red" }}
+                    >
+                      x
+                    </p>
                   </div>
                 )
               })}
             </div>
             <div style={column}>
-              <button onClick={this.clearSelectedFeatures}>Clear</button>
+              <button onClick={() => this.clearSelectedFeatures()}>Clear</button>
+            </div>
+          </div>
+
+          <div style={column}>
+            <p>Possible Minerals:</p>
+            <div style={{ display: "flex", flexWrap: "wrap", flexDirection: "column" }}>
+              {/*this.state.outputArr.sort(outputCompare).map((output: Output) => {
+                return (
+                  <div title={output.Rock.description} style={{ ...column }}>
+                    <p>
+                      {output.Rock.name} - {output.ConfidenceScore}
+                    </p>
+                  </div>
+                )
+              })*/}
+
+              <button
+                type={"button"}
+                onClick={() =>
+                  this.setState({
+                    selectedOutput:
+                      this.state.selectedOutput > 0 ? this.state.selectedOutput - 1 : this.state.selectedOutput,
+                  })
+                }
+              >
+                ^^^
+              </button>
+              {this.possibleRocks()}
+              <button type={"button"} onClick={() => this.setState({ selectedOutput: this.state.selectedOutput + 1 })}>
+                vvv
+              </button>
             </div>
           </div>
         </div>
