159fdf995e380fd20f4d68e4483c40d93c613ac4
ThisIsBrady <bradyh2001@gmail.com>
2022-08-05 23:13:30 -0500

    Format RAM Science

--- src/RAM/Science/components/Fluorometer.tsx
+++ src/RAM/Science/components/Fluorometer.tsx
@@ -1,86 +1,97 @@
-import React, { Component } from "react"
-import CSS from "csstype"
-import { rovecomm } from "../../../Core/RoveProtocol/Rovecomm"
-import fs from "fs"
+import React, { Component } from 'react';
+import CSS from 'csstype';
+import fs from 'fs';
+import { rovecomm } from '../../../Core/RoveProtocol/Rovecomm';
 
 const container: CSS.Properties = {
-  display: "flex",
-  flexDirection: "column",
-  fontFamily: "arial",
-  borderTopWidth: "28px",
-  borderColor: "#990000",
-  borderBottomWidth: "2px",
-  borderStyle: "solid",
-  height: "calc(100% - 40px)",
-  padding: "5px",
-}
+  display: 'flex',
+  flexDirection: 'column',
+  fontFamily: 'arial',
+  borderTopWidth: '28px',
+  borderColor: '#990000',
+  borderBottomWidth: '2px',
+  borderStyle: 'solid',
+  height: 'calc(100% - 40px)',
+  padding: '5px',
+};
 const label: CSS.Properties = {
-  marginTop: "-10px",
-  position: "relative",
-  top: "24px",
-  left: "3px",
-  fontFamily: "arial",
-  fontSize: "16px",
+  marginTop: '-10px',
+  position: 'relative',
+  top: '24px',
+  left: '3px',
+  fontFamily: 'arial',
+  fontSize: '16px',
   zIndex: 1,
-  color: "white",
-}
+  color: 'white',
+};
 const row: CSS.Properties = {
-  display: "flex",
-  flexDirection: "row",
+  display: 'flex',
+  flexDirection: 'row',
   flexGrow: 1,
-  justifyContent: "space-around",
-  alignContent: "center",
-  marginTop: "5px",
-  width: "100%",
-}
+  justifyContent: 'space-around',
+  alignContent: 'center',
+  marginTop: '5px',
+  width: '100%',
+};
 const componentBox: CSS.Properties = {
-  margin: "3px 0 3px 0",
-}
+  margin: '3px 0 3px 0',
+};
 const button: CSS.Properties = {
-  marginLeft: "15px",
-  width: "60px",
-  alignSelf: "center",
-}
+  marginLeft: '15px',
+  width: '60px',
+  alignSelf: 'center',
+};
 
 const controlButton: CSS.Properties = {
-  //margin: "5px",
-}
+  // margin: "5px",
+};
 
 /** Will be merged with the row css if the Laser is off */
 const offIndicator: CSS.Properties = {
-  backgroundColor: "#FF0000",
-  lineHeight: "27px",
-}
+  backgroundColor: '#FF0000',
+  lineHeight: '27px',
+};
 
 /** Will be merged with the row css if the Laser is off */
 const onIndicator: CSS.Properties = {
-  backgroundColor: "#00FF00",
-  lineHeight: "27px",
-}
+  backgroundColor: '#00FF00',
+  lineHeight: '27px',
+};
 
 interface IProps {
-  style?: CSS.Properties
+  style?: CSS.Properties;
 }
 
 interface IState {
   /** Holds the last sent values of the diodes */
-  DiodeValues: number[]
+  DiodeValues: number[];
   /** Holds which lasers are enabled */
-  LasersPowered: boolean[]
+  LasersPowered: boolean[];
 }
 
 class Fluorometer extends Component<IProps, IState> {
+  static defaultProps = {
+    style: {},
+  };
+
+  static buildLaserCommand(Lasers: boolean[]): number {
+    let bitmask = '';
+    bitmask += Lasers[0] ? '1' : '0';
+    bitmask += Lasers[1] ? '1' : '0';
+    bitmask += Lasers[2] ? '1' : '0';
+    return parseInt(bitmask, 2);
+  }
+
   constructor(props: IProps) {
-    super(props)
+    super(props);
     this.state = {
       DiodeValues: [0, 0, 0],
       LasersPowered: [false, false, false],
-    }
-    this.updateDiodeVals = this.updateDiodeVals.bind(this)
-    this.buildLaserCommand = this.buildLaserCommand.bind(this)
-    this.exportData = this.exportData.bind(this)
+    };
+    this.updateDiodeVals = this.updateDiodeVals.bind(this);
+    this.exportData = this.exportData.bind(this);
 
-    rovecomm.on("FluorometerData", (data: any) => this.updateDiodeVals(data))
+    rovecomm.on('FluorometerData', (data: any) => this.updateDiodeVals(data));
   }
 
   /**
@@ -88,45 +99,37 @@ class Fluorometer extends Component<IProps, IState> {
    * @param data float array of length 3 with the new data
    */
   updateDiodeVals(data: number[]): void {
-    this.setState({ DiodeValues: [data[0], data[1], data[2]] })
-  }
-
-  buildLaserCommand(Lasers: boolean[]): number {
-    let bitmask = ""
-    bitmask += Lasers[0] ? "1" : "0"
-    bitmask += Lasers[1] ? "1" : "0"
-    bitmask += Lasers[2] ? "1" : "0"
-    return parseInt(bitmask, 2)
+    this.setState({ DiodeValues: [data[0], data[1], data[2]] });
   }
 
   toggleLaser(index: number): void {
-    const { LasersPowered } = this.state
-    LasersPowered[index] = !LasersPowered[index]
-    rovecomm.sendCommand("FLasers", [this.buildLaserCommand(LasersPowered)])
-    this.setState({ LasersPowered })
+    const { LasersPowered } = this.state;
+    LasersPowered[index] = !LasersPowered[index];
+    rovecomm.sendCommand('FLasers', [Fluorometer.buildLaserCommand(LasersPowered)]);
+    this.setState({ LasersPowered });
   }
 
   exportData(): void {
     // ISO string will be fromatted YYYY-MM-DDTHH:MM:SS:sssZ
     // this regex will convert all -,T,:,Z to . (which covers to . for .csv)
     // Date format is consistent with the SensorData csv
-    const timestamp = new Date().toISOString().replaceAll(/[:\-TZ]/g, ".")
-    const EXPORT_FILE = `./ScienceSaveFiles/Fluormeter-${timestamp}.csv`
+    const timestamp = new Date().toISOString().replaceAll(/[:\-TZ]/g, '.');
+    const EXPORT_FILE = `./ScienceSaveFiles/Fluormeter-${timestamp}.csv`;
 
-    const { DiodeValues, LasersPowered } = this.state
+    const { DiodeValues, LasersPowered } = this.state;
 
-    if (!fs.existsSync("./ScienceSaveFiles")) {
-      fs.mkdirSync("./ScienceSaveFiles")
+    if (!fs.existsSync('./ScienceSaveFiles')) {
+      fs.mkdirSync('./ScienceSaveFiles');
     }
-    let csvText = "Laser1,Laser2,Laser3,Diode1 (nm),Diode2 (nm),Diode3 (nm)\n"
-    csvText += `${LasersPowered[0] ? "On" : "Off"},${LasersPowered[1] ? "On" : "Off"},${
-      LasersPowered[2] ? "On" : "Off"
-    },`
-    csvText += `${DiodeValues[0]},${DiodeValues[1]},${DiodeValues[2]},\n`
+    let csvText = 'Laser1,Laser2,Laser3,Diode1 (nm),Diode2 (nm),Diode3 (nm)\n';
+    csvText += `${LasersPowered[0] ? 'On' : 'Off'},${LasersPowered[1] ? 'On' : 'Off'},${
+      LasersPowered[2] ? 'On' : 'Off'
+    },`;
+    csvText += `${DiodeValues[0]},${DiodeValues[1]},${DiodeValues[2]},\n`;
 
-    fs.writeFile(EXPORT_FILE, csvText, err => {
-      if (err) throw err
-    })
+    fs.writeFile(EXPORT_FILE, csvText, (err) => {
+      if (err) throw err;
+    });
   }
 
   render(): JSX.Element {
@@ -138,11 +141,9 @@ class Fluorometer extends Component<IProps, IState> {
             {this.state.DiodeValues.map((value, index) => {
               return (
                 <div key={index} style={row}>
-                  <label>
-                    Diode {index + 1}: {value.toFixed(3)} nm
-                  </label>
+                  Diode {index + 1}: {value.toFixed(3)} nm
                 </div>
-              )
+              );
             })}
           </div>
           <div style={componentBox}>
@@ -150,17 +151,14 @@ class Fluorometer extends Component<IProps, IState> {
               return (
                 <div
                   key={index}
-                  style={{ ...row, ...(value ? onIndicator : offIndicator), justifyContent: "space-between" }}
+                  style={{ ...row, ...(value ? onIndicator : offIndicator), justifyContent: 'space-between' }}
                 >
-                  <label style={{ alignSelf: "center", fontWeight: "bold", marginLeft: "5px" }}>
-                    {" "}
-                    Laser {index + 1}:{" "}
-                  </label>
-                  <button style={{ ...button, marginRight: "5px" }} onClick={() => this.toggleLaser(index)}>
-                    {value ? "Disable" : "Enable"}
+                  <p style={{ alignSelf: 'center', fontWeight: 'bold', marginLeft: '5px' }}> Laser {index + 1}: </p>
+                  <button style={{ ...button, marginRight: '5px' }} onClick={() => this.toggleLaser(index)}>
+                    {value ? 'Disable' : 'Enable'}
                   </button>
                 </div>
-              )
+              );
             })}
           </div>
           <div style={componentBox}>
@@ -172,8 +170,8 @@ class Fluorometer extends Component<IProps, IState> {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Fluorometer
+export default Fluorometer;
--- src/RAM/Science/components/Heater.tsx
+++ src/RAM/Science/components/Heater.tsx
@@ -1,87 +1,98 @@
-import React, { Component } from "react"
-import CSS from "csstype"
-import { rovecomm } from "../../../Core/RoveProtocol/Rovecomm"
-import { BitmaskUnpack } from "../../../Core/BitmaskUnpack"
+import React, { Component } from 'react';
+import CSS from 'csstype';
+import { rovecomm } from '../../../Core/RoveProtocol/Rovecomm';
+import { BitmaskUnpack } from '../../../Core/BitmaskUnpack';
 
 const container: CSS.Properties = {
-  display: "flex",
-  flexDirection: "column",
-  fontFamily: "arial",
-  borderTopWidth: "28px",
-  borderColor: "#990000",
-  borderBottomWidth: "2px",
-  borderStyle: "solid",
-  padding: "5px",
-}
+  display: 'flex',
+  flexDirection: 'column',
+  fontFamily: 'arial',
+  borderTopWidth: '28px',
+  borderColor: '#990000',
+  borderBottomWidth: '2px',
+  borderStyle: 'solid',
+  padding: '5px',
+};
 const label: CSS.Properties = {
-  marginTop: "-10px",
-  position: "relative",
-  top: "24px",
-  left: "3px",
-  fontFamily: "arial",
-  fontSize: "16px",
+  marginTop: '-10px',
+  position: 'relative',
+  top: '24px',
+  left: '3px',
+  fontFamily: 'arial',
+  fontSize: '16px',
   zIndex: 1,
-  color: "white",
-}
+  color: 'white',
+};
 const row: CSS.Properties = {
-  display: "flex",
-  flexDirection: "row",
+  display: 'flex',
+  flexDirection: 'row',
   flexGrow: 1,
-  justifyContent: "space-around",
-  lineHeight: "25px",
-  margin: "2.5px",
-}
+  justifyContent: 'space-around',
+  lineHeight: '25px',
+  margin: '2.5px',
+};
 const column: CSS.Properties = {
-  display: "flex",
-  flexDirection: "column",
+  display: 'flex',
+  flexDirection: 'column',
   flexGrow: 1,
-  justifyContent: "space-around",
-}
+  justifyContent: 'space-around',
+};
 const blockLabel: CSS.Properties = {
-  width: "100%",
-  justifyContent: "space-around",
-  alignSelf: "center",
-  marginLeft: "5px",
-  fontWeight: "bold",
-}
+  width: '100%',
+  justifyContent: 'space-around',
+  alignSelf: 'center',
+  marginLeft: '5px',
+  fontWeight: 'bold',
+};
 
 const button: CSS.Properties = {
-  width: "150px",
-  height: "25px",
-  alignSelf: "center",
-  margin: "0 5px 0 5px",
-}
+  width: '150px',
+  height: '25px',
+  alignSelf: 'center',
+  margin: '0 5px 0 5px',
+};
 
 /** Will be merged with the row css if the block is off */
 const offIndicator: CSS.Properties = {
-  backgroundColor: "#FF0000",
-}
+  backgroundColor: '#FF0000',
+};
 
 /** Will be merged with the row css if the block is off */
 const onIndicator: CSS.Properties = {
-  backgroundColor: "#00FF00",
-}
+  backgroundColor: '#00FF00',
+};
 
 type HeaterBlock = {
   /** The current temperature of the block in Celsius */
-  temp: number
+  temp: number;
   /** True if the heater block is turned on */
-  isOn: boolean
-}
+  isOn: boolean;
+};
 
 interface IProps {
-  style?: CSS.Properties
+  style?: CSS.Properties;
 }
 
 interface IState {
-  blocks: HeaterBlock[]
-  UVPowered: boolean
-  WhiteLightPowered: boolean
+  blocks: HeaterBlock[];
+  UVPowered: boolean;
+  WhiteLightPowered: boolean;
 }
 
 class Heater extends Component<IProps, IState> {
+  static defaultProps = {
+    style: {},
+  };
+
+  static buildLightCommand(UV: boolean, White: boolean): number {
+    let bitmask = '';
+    bitmask += UV ? '1' : '0';
+    bitmask += White ? '1' : '0';
+    return parseInt(bitmask, 2);
+  }
+
   constructor(props: IProps) {
-    super(props)
+    super(props);
     this.state = {
       UVPowered: false,
       WhiteLightPowered: false,
@@ -99,76 +110,15 @@ class Heater extends Component<IProps, IState> {
           isOn: false,
         },
       ],
-    }
-    this.updateTemps = this.updateTemps.bind(this)
-    this.toggleBlock = this.toggleBlock.bind(this)
-    this.updateEnabled = this.updateEnabled.bind(this)
-    this.setAllBlocks = this.setAllBlocks.bind(this)
-    this.toggleWhiteLight = this.toggleWhiteLight.bind(this)
-    this.toggleUV = this.toggleUV.bind(this)
-    this.buildLightCommand = this.buildLightCommand.bind(this)
-    rovecomm.on("ThermoValues", (data: any) => this.updateTemps(data))
-    rovecomm.on("HeaterEnabled", (data: any) => this.updateEnabled(data))
-  }
-
-  buildLightCommand(UV: boolean, White: boolean): number {
-    let bitmask = ""
-    bitmask += UV ? "1" : "0"
-    bitmask += White ? "1" : "0"
-    return parseInt(bitmask, 2)
-  }
-
-  toggleWhiteLight(): void {
-    this.setState({ WhiteLightPowered: !this.state.WhiteLightPowered }, () => {
-      rovecomm.sendCommand("Lights", [this.buildLightCommand(this.state.UVPowered, this.state.WhiteLightPowered)])
-    })
-  }
-
-  toggleUV(): void {
-    this.setState({ UVPowered: !this.state.UVPowered }, () => {
-      rovecomm.sendCommand("Lights", [this.buildLightCommand(this.state.UVPowered, this.state.WhiteLightPowered)])
-    })
-  }
-
-  updateEnabled(data: number[]): void {
-    const { blocks } = this.state
-    const bitmask = BitmaskUnpack(data[0], blocks.length)
-    for (var i: number = 0; i < blocks.length; i++) {
-      //subtracted from the length since we have to reverse the order of the block
-      blocks[blocks.length - 1 - i].isOn = Boolean(Number(bitmask[i]))
-    }
-    this.setState({ blocks })
-  }
-
-  /**
-   * Updates the temperatures shown on the component
-   * @param temps a three value array of temps in degrees C
-   */
-  updateTemps(temps: number[]): void {
-    const { blocks } = this.state
-
-    for (let i: number = 0; i < this.state.blocks.length; i++) {
-      blocks[i].temp = temps[i]
-    }
-
-    this.setState({ blocks })
-  }
-
-  /**
-   * Sends the command to toggle one block
-   * Does **not** update the state of the component. The board will send when it has turned off
-   * @param index 0-based index of the block to toggle
-   */
-  toggleBlock(index: number): void {
-    const blocks: HeaterBlock[] = JSON.parse(JSON.stringify(this.state.blocks)) //Make a new array with copied data in order to not change state
-    blocks[index].isOn = !blocks[index].isOn
-
-    let bitmask = ""
-    for (let i: number = blocks.length - 1; i >= 0; i--) {
-      //Reverse for-loop since the order of the blocks is reversed on the board
-      bitmask += blocks[i].isOn ? "1" : "0"
-    }
-    rovecomm.sendCommand("HeaterToggle", [parseInt(bitmask, 2)])
+    };
+    this.updateTemps = this.updateTemps.bind(this);
+    this.toggleBlock = this.toggleBlock.bind(this);
+    this.updateEnabled = this.updateEnabled.bind(this);
+    this.setAllBlocks = this.setAllBlocks.bind(this);
+    this.toggleWhiteLight = this.toggleWhiteLight.bind(this);
+    this.toggleUV = this.toggleUV.bind(this);
+    rovecomm.on('ThermoValues', (data: any) => this.updateTemps(data));
+    rovecomm.on('HeaterEnabled', (data: any) => this.updateEnabled(data));
   }
 
   /**
@@ -177,26 +127,79 @@ class Heater extends Component<IProps, IState> {
    * @param powered true to turn all on, false to turn all off
    */
   setAllBlocks(powered: boolean): void {
-    let bitmask = powered ? "1" : "0"
-    bitmask = bitmask.repeat(this.state.blocks.length)
-    rovecomm.sendCommand("HeaterToggle", [parseInt(bitmask, 2)])
+    let bitmask = powered ? '1' : '0';
+    bitmask = bitmask.repeat(this.state.blocks.length);
+    rovecomm.sendCommand('HeaterToggle', [parseInt(bitmask, 2)]);
+  }
+
+  toggleWhiteLight(): void {
+    this.setState({ WhiteLightPowered: !this.state.WhiteLightPowered }, () => {
+      rovecomm.sendCommand('Lights', [Heater.buildLightCommand(this.state.UVPowered, this.state.WhiteLightPowered)]);
+    });
+  }
+
+  toggleUV(): void {
+    this.setState({ UVPowered: !this.state.UVPowered }, () => {
+      rovecomm.sendCommand('Lights', [Heater.buildLightCommand(this.state.UVPowered, this.state.WhiteLightPowered)]);
+    });
+  }
+
+  updateEnabled(data: number[]): void {
+    const { blocks } = this.state;
+    const bitmask = BitmaskUnpack(data[0], blocks.length);
+    for (let i = 0; i < blocks.length; i++) {
+      // subtracted from the length since we have to reverse the order of the block
+      blocks[blocks.length - 1 - i].isOn = Boolean(Number(bitmask[i]));
+    }
+    this.setState({ blocks });
+  }
+
+  /**
+   * Updates the temperatures shown on the component
+   * @param temps a three value array of temps in degrees C
+   */
+  updateTemps(temps: number[]): void {
+    const { blocks } = this.state;
+
+    for (let i = 0; i < this.state.blocks.length; i++) {
+      blocks[i].temp = temps[i];
+    }
+
+    this.setState({ blocks });
+  }
+
+  /**
+   * Sends the command to toggle one block
+   * Does **not** update the state of the component. The board will send when it has turned off
+   * @param index 0-based index of the block to toggle
+   */
+  toggleBlock(index: number): void {
+    const blocks: HeaterBlock[] = JSON.parse(JSON.stringify(this.state.blocks)); // Make a new array with copied data in order to not change state
+    blocks[index].isOn = !blocks[index].isOn;
+
+    let bitmask = '';
+    for (let i: number = blocks.length - 1; i >= 0; i--) {
+      // Reverse for-loop since the order of the blocks is reversed on the board
+      bitmask += blocks[i].isOn ? '1' : '0';
+    }
+    rovecomm.sendCommand('HeaterToggle', [parseInt(bitmask, 2)]);
   }
 
   render(): JSX.Element {
     return (
-      <div id={"Heater"} style={this.props.style}>
+      <div id="Heater" style={this.props.style}>
         <div style={label}>Science Hardware</div>
         <div style={container}>
           <div style={column}>
             {this.state.blocks.map((block, index) => {
               return (
-                <div style={{ ...row, ...(block.isOn ? onIndicator : offIndicator) }}>
-                  <label style={blockLabel}>Block {index + 1}: </label>
+                <div key={index} style={{ ...row, ...(block.isOn ? onIndicator : offIndicator) }}>
+                  <p style={blockLabel}>Block {index + 1}: </p>
                   <button style={button} onClick={() => this.toggleBlock(index)}>
                     {block.temp.toFixed(2)}&#176; C
                   </button>
                 </div>
-              )
+              );
             })}
             <div style={row}>
               <button style={button} onClick={() => this.setAllBlocks(true)}>
@@ -208,51 +211,55 @@ class Heater extends Component<IProps, IState> {
             </div>
             <div style={row}>
               <div>
-                <input
-                  type="checkbox"
-                  id="WhiteCheck"
-                  name="WhiteCheck"
-                  onChange={() => this.toggleWhiteLight()}
-                  checked={this.state.WhiteLightPowered}
-                />
-                <label htmlFor="WhiteCheck">White Light</label>
+                <label htmlFor="WhiteCheck">
+                  <input
+                    type="checkbox"
+                    id="WhiteCheck"
+                    name="WhiteCheck"
+                    onChange={() => this.toggleWhiteLight()}
+                    checked={this.state.WhiteLightPowered}
+                  />
+                  White Light
+                </label>
               </div>
               <div>
-                <input
-                  type="checkbox"
-                  id="UVCheck"
-                  name="UVCheck"
-                  onChange={() => this.toggleUV()}
-                  checked={this.state.UVPowered}
-                />
-                <label htmlFor="UVCheck">UV Light</label>
+                <label htmlFor="UVCheck">
+                  <input
+                    type="checkbox"
+                    id="UVCheck"
+                    name="UVCheck"
+                    onChange={() => this.toggleUV()}
+                    checked={this.state.UVPowered}
+                  />
+                  UV Light
+                </label>
               </div>
             </div>
             <div style={row}>
               <button
                 onClick={() => {
-                  rovecomm.sendCommand("GotoPosition", 0)
+                  rovecomm.sendCommand('GotoPosition', 0);
                 }}
               >
                 Scooping Position
               </button>
               <button
                 onClick={() => {
-                  rovecomm.sendCommand("GotoPosition", 1)
+                  rovecomm.sendCommand('GotoPosition', 1);
                 }}
               >
                 Sample 1
               </button>
               <button
                 onClick={() => {
-                  rovecomm.sendCommand("GotoPosition", 2)
+                  rovecomm.sendCommand('GotoPosition', 2);
                 }}
               >
                 Sample 2
               </button>
               <button
                 onClick={() => {
-                  rovecomm.sendCommand("GotoPosition", 0)
+                  rovecomm.sendCommand('GotoPosition', 0);
                 }}
               >
                 Sample 3
@@ -261,8 +268,8 @@ class Heater extends Component<IProps, IState> {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Heater
+export default Heater;
