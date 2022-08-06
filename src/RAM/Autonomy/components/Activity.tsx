42bee6403f7dc40e2c5c7715d96572e10fbc29ec
ThisIsBrady <bradyh2001@gmail.com>
2022-08-05 23:13:17 -0500

    Format RAM Autonomy and RAM page

--- src/RAM/Autonomy/components/Activity.tsx
+++ src/RAM/Autonomy/components/Activity.tsx
@@ -1,73 +1,75 @@
-import React, { Component } from "react"
-import CSS from "csstype"
-import { rovecomm } from "../../../Core/RoveProtocol/Rovecomm"
+import React, { Component } from 'react';
+import CSS from 'csstype';
+import { rovecomm } from '../../../Core/RoveProtocol/Rovecomm';
 
 const container: CSS.Properties = {
-  display: "flex",
-  flexDirection: "column",
-  fontFamily: "arial",
-  borderTopWidth: "28px",
-  borderColor: "#990000",
-  borderBottomWidth: "2px",
-  borderStyle: "solid",
-  whiteSpace: "pre-wrap",
-  overflow: "scroll",
-  height: "calc(100% - 40px)",
-}
+  display: 'flex',
+  flexDirection: 'column',
+  fontFamily: 'arial',
+  borderTopWidth: '28px',
+  borderColor: '#990000',
+  borderBottomWidth: '2px',
+  borderStyle: 'solid',
+  whiteSpace: 'pre-wrap',
+  overflow: 'scroll',
+  height: 'calc(100% - 40px)',
+};
 const label: CSS.Properties = {
-  marginTop: "-10px",
-  position: "relative",
-  top: "24px",
-  left: "3px",
-  fontFamily: "arial",
-  fontSize: "16px",
+  marginTop: '-10px',
+  position: 'relative',
+  top: '24px',
+  left: '3px',
+  fontFamily: 'arial',
+  fontSize: '16px',
   zIndex: 1,
-  color: "white",
-}
+  color: 'white',
+};
 
 interface IProps {
-  style?: CSS.Properties
+  style?: CSS.Properties;
 }
 
 interface IState {
-  ActivityText: string
-  backgroundColor: string
+  ActivityText: string;
+  backgroundColor: string;
 }
 
 class Activity extends Component<IProps, IState> {
+  static defaultProps = {
+    style: {},
+  };
+
   constructor(props: IProps) {
-    super(props)
+    super(props);
     this.state = {
-      ActivityText: "",
-      backgroundColor: "white",
-    }
+      ActivityText: '',
+      backgroundColor: 'white',
+    };
 
-    this.ReachedMarker = this.ReachedMarker.bind(this)
+    this.ReachedMarker = this.ReachedMarker.bind(this);
+    this.Log = this.Log.bind(this);
 
-    rovecomm.on("CurrentLog", (data: any) => this.Log(data))
-    rovecomm.on("ReachedMarker", this.ReachedMarker)
+    rovecomm.on('CurrentLog', (data: any) => this.Log(data));
+    rovecomm.on('ReachedMarker', this.ReachedMarker);
   }
 
   Log(data: string): void {
-    let text = this.state.ActivityText
-    text += `${new Date().toLocaleTimeString()}: ${data} \n` // set this to be that variable and time and newline
-    this.setState({
-      ActivityText: text,
-    })
+    this.setState((prevState) => ({
+      ActivityText: `${prevState.ActivityText}${new Date().toLocaleTimeString()}: ${data} \n`,
+    }));
   }
 
   ReachedMarker(): void {
-    this.setState({ backgroundColor: "green" })
-    this.Log("Reached waypoint!")
+    this.setState({ backgroundColor: 'green' });
+    this.Log('Reached waypoint!');
     const reachInterval = setInterval(() => {
-      const backgroundColor: string = this.state.backgroundColor === "green" ? "white" : "green"
-      this.setState({ backgroundColor })
-    }, 250)
+      this.setState((prevState) => ({ backgroundColor: prevState.backgroundColor === 'green' ? 'white' : 'green' }));
+    }, 250);
 
     setTimeout(() => {
-      clearInterval(reachInterval)
-      this.setState({ backgroundColor: "white" })
-    }, 4000)
+      clearInterval(reachInterval);
+      this.setState({ backgroundColor: 'white' });
+    }, 4000);
   }
 
   render(): JSX.Element {
@@ -76,8 +78,8 @@ class Activity extends Component<IProps, IState> {
         <div style={label}>Autonomy Activity</div>
         <div style={{ ...container, backgroundColor: this.state.backgroundColor }}>{this.state.ActivityText}</div>
       </div>
-    )
+    );
   }
 }
 
-export default Activity
+export default Activity;
--- src/RAM/Autonomy/components/Lighting.tsx
+++ src/RAM/Autonomy/components/Lighting.tsx
@@ -1,89 +1,93 @@
-import React, { Component } from "react"
-import CSS from "csstype"
-import { ChromePicker } from "react-color"
-import { rovecomm, RovecommManifest } from "../../../Core/RoveProtocol/Rovecomm"
+import React, { Component } from 'react';
+import CSS from 'csstype';
+import { ChromePicker } from 'react-color';
+import { rovecomm, RovecommManifest } from '../../../Core/RoveProtocol/Rovecomm';
 
 const container: CSS.Properties = {
-  display: "flex",
-  fontFamily: "arial",
-  borderTopWidth: "28px",
-  borderColor: "#990000",
-  borderBottomWidth: "2px",
-  borderStyle: "solid",
-  flexDirection: "column",
-}
+  display: 'flex',
+  fontFamily: 'arial',
+  borderTopWidth: '28px',
+  borderColor: '#990000',
+  borderBottomWidth: '2px',
+  borderStyle: 'solid',
+  flexDirection: 'column',
+};
 const label: CSS.Properties = {
-  marginTop: "-10px",
-  position: "relative",
-  top: "24px",
-  left: "3px",
-  fontFamily: "arial",
-  fontSize: "16px",
+  marginTop: '-10px',
+  position: 'relative',
+  top: '24px',
+  left: '3px',
+  fontFamily: 'arial',
+  fontSize: '16px',
   zIndex: 1,
-  color: "white",
-}
+  color: 'white',
+};
 
 const row: CSS.Properties = {
-  display: "flex",
-  flexDirection: "row",
-}
+  display: 'flex',
+  flexDirection: 'row',
+};
 
 const column: CSS.Properties = {
-  display: "flex",
-  flexDirection: "column",
-}
+  display: 'flex',
+  flexDirection: 'column',
+};
 
 const button: CSS.Properties = {
-  margin: "5px",
-}
+  margin: '5px',
+};
 
 interface RGBColor {
-  r: number
-  g: number
-  b: number
-  a: number
+  r: number;
+  g: number;
+  b: number;
+  a: number;
 }
 
 interface IProps {
-  style?: CSS.Properties
+  style?: CSS.Properties;
 }
 interface IState {
-  color: RGBColor
-  brightInput: string
+  color: RGBColor;
+  brightInput: string;
 }
 
 class Lighting extends Component<IProps, IState> {
+  static defaultProps = {
+    style: {},
+  };
+
+  static teleop(): void {
+    rovecomm.sendCommand('StateDisplay', RovecommManifest.Multimedia.Enums.DISPLAYSTATE.Teleop);
+    console.log(RovecommManifest.Multimedia.Enums.DISPLAYSTATE.Teleop);
+  }
+
+  static autonomy(): void {
+    rovecomm.sendCommand('StateDisplay', RovecommManifest.Multimedia.Enums.DISPLAYSTATE.Autonomy);
+    console.log(RovecommManifest.Multimedia.Enums.DISPLAYSTATE.Autonomy);
+  }
+
+  static reachedGoal(): void {
+    rovecomm.sendCommand('StateDisplay', RovecommManifest.Multimedia.Enums.DISPLAYSTATE.Reached_Goal);
+    console.log(RovecommManifest.Multimedia.Enums.DISPLAYSTATE.Reached_Goal);
+  }
+
   constructor(props: IProps) {
-    super(props)
+    super(props);
     this.state = {
       color: { r: 255, g: 255, b: 255, a: 0 },
-      brightInput: "",
-    }
+      brightInput: '',
+    };
   }
 
   handleEdit(event: any): void {
-    this.setState({ brightInput: event.target.value })
+    this.setState({ brightInput: event.target.value });
   }
 
   colorChanged(newColor: any): void {
-    const color = newColor.rgb
-    this.setState({ color })
-    rovecomm.sendCommand("LEDRGB", [color.r, color.g, color.b])
-  }
-
-  teleop(): void {
-    rovecomm.sendCommand("StateDisplay", RovecommManifest.Multimedia.Enums.DISPLAYSTATE.Teleop)
-    console.log(RovecommManifest.Multimedia.Enums.DISPLAYSTATE.Teleop)
-  }
-
-  autonomy(): void {
-    rovecomm.sendCommand("StateDisplay", RovecommManifest.Multimedia.Enums.DISPLAYSTATE.Autonomy)
-    console.log(RovecommManifest.Multimedia.Enums.DISPLAYSTATE.Autonomy)
-  }
-
-  reachedGoal(): void {
-    rovecomm.sendCommand("StateDisplay", RovecommManifest.Multimedia.Enums.DISPLAYSTATE.Reached_Goal)
-    console.log(RovecommManifest.Multimedia.Enums.DISPLAYSTATE.Reached_Goal)
+    const color = newColor.rgb;
+    this.setState({ color });
+    rovecomm.sendCommand('LEDRGB', [color.r, color.g, color.b]);
   }
 
   render(): JSX.Element {
@@ -93,29 +97,29 @@ class Lighting extends Component<IProps, IState> {
         <div style={container}>
           <div style={column}>
             <ChromePicker color={this.state.color} onChangeComplete={(color: any) => this.colorChanged(color)} />
-            <div style={{ ...row, justifyContent: "center" }}>
-              <button style={button} onClick={() => this.teleop()}>
+            <div style={{ ...row, justifyContent: 'center' }}>
+              <button style={button} onClick={Lighting.teleop}>
                 Teleop
               </button>
-              <button style={button} onClick={() => this.autonomy()}>
+              <button style={button} onClick={Lighting.autonomy}>
                 Autonomy
               </button>
-              <button style={button} onClick={() => this.reachedGoal()}>
+              <button style={button} onClick={Lighting.reachedGoal}>
                 Goal
               </button>
             </div>
-            <div style={{ ...row, justifyContent: "center", marginBottom: "5px" }}>
+            <div style={{ ...row, justifyContent: 'center', marginBottom: '5px' }}>
               <input
-                style={{ width: "50px" }}
+                style={{ width: '50px' }}
                 id="bright"
                 value={this.state.brightInput}
-                onChange={e => this.handleEdit(e)}
-              ></input>
+                onChange={(e) => this.handleEdit(e)}
+              />
               <button
                 type="button"
                 onClick={() => {
                   if (Number(this.state.brightInput) >= 0 && Number(this.state.brightInput) <= 124)
-                    rovecomm.sendCommand("Brightness", this.state.brightInput)
+                    rovecomm.sendCommand('Brightness', this.state.brightInput);
                 }}
               >
                 Set Brightness
@@ -124,8 +128,8 @@ class Lighting extends Component<IProps, IState> {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Lighting
+export default Lighting;
