748302e9e6800793bbeb35e06ea9bb6668617890
Gabriel Workman <glwh4p@umsystem.edu>
2024-03-19 21:31:19 -0500

    added location and battery display to drone panel

--- src/RAM/Drone/Drone.tsx
+++ src/RAM/Drone/Drone.tsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import CSS from 'csstype';
-import DroneControls from './components/drone_controls';
-import DroneState from './components/drone_state';
-import DroneActivity from './components/drone_activity';
-import DroneTeleop from './components/drone_teleop';
+import DroneBattery from './components/drone_battery';
+// import DroneCamera from './components/drone_camera';
+import DroneLocation from './components/drone_location';
+// import DroneMap from './components/drone_map';
+// import ThreeDdrone from './components/ThreeD_drone';
 
 const row: CSS.Properties = {
   display: 'flex',
@@ -30,15 +31,15 @@ class Drone extends Component<IProps, IState> {
     return (
       <div style={row}>
         <div style={{ ...column, flex: 1 }}>
-          <DroneControls
-            style={{ marginRight: '5px', flexWrap: 'wrap' }}
-            selectedWaypoint={this.props.selectedWaypoint}
-          />
-          <DroneActivity style={{ marginRight: '5px' }} />
-          <DroneTeleop style={{ marginRight: '5px' }} />
+          {/* <DroneMap style={{ marginRight: '5px', flexWrap: 'wrap' }}/> */}
+          {/* <ThreeDdrone/> */}
         </div>
-        <div style={{ ...column, width: '20%' }}>
-          <DroneState />
+        <div style={row}>
+          <DroneLocation/>
+          <DroneBattery/>
+        </div>
+        <div style={row}>
+          {/* <DroneCamera/> */}
         </div>
       </div>
     );
