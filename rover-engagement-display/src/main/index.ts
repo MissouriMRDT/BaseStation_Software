8ff0d3e6eb458fa980e99b02781c51704b067aa3
Donovan Bale <donovan@balehaus.org>
2024-03-22 03:06:22 -0500

    asdfasdfasdfasdfsdfasd

--- rover-engagement-display/src/main/index.ts
+++ rover-engagement-display/src/main/index.ts
@@ -2,7 +2,7 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
-import { rovecomm } from './Core/RoveProtocol/Rovecomm'
+import { rovecomm } from './Core/RoveComm-TypeScript/RoveProtocol/Rovecomm'
 
 function createWindow(): void {
   // Create the browser window.
