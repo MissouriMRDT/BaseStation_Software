4261d1c7f0b4ed9c76192099bfff396489d13f1d
OcelotEmpire <hobbz.pi@gmail.com>
2024-03-27 01:39:15 -0500

    allowed switching between stl and glb files

--- src/Core/components/Accelerometer.tsx
+++ src/Core/components/Accelerometer.tsx
@@ -5,7 +5,7 @@ import { rovecomm } from '../RoveProtocol/Rovecomm';
 import { ArrowHelperProps, Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
-import { BufferGeometry, Euler, Group, Object3DEventMap, Vector3 } from 'three';
+import { BufferGeometry, Euler, Quaternion, Vector3 } from 'three';
 import { RoverScene } from './RoverScene';
 
 const container: CSS.Properties = {
@@ -32,7 +32,9 @@ const label: CSS.Properties = {
   color: 'white',
 };
 
-const ROVER_FILE = path.join(__dirname, '../assets/models/Rover.stl');
+export const MODELS_PATH = path.join(__dirname, '../assets/models');
+
+const negativeY = new Vector3(0, -1, 0);
 
 interface IProps {
   style?: CSS.Properties;
@@ -44,8 +46,8 @@ interface IState {
   rotation: Euler;
   width: number;
   height: number;
+  file: string;
   geometry?: BufferGeometry;
-  scene?: Group<Object3DEventMap>;
 }
 
 class Accelerometer extends Component<IProps, IState> {
@@ -55,6 +57,7 @@ class Accelerometer extends Component<IProps, IState> {
   };
 
   private arrowRef: React.RefObject<ArrowHelperProps>;
+  private showUpdate = false;
 
   constructor(props: IProps) {
     super(props);
@@ -63,6 +66,7 @@ class Accelerometer extends Component<IProps, IState> {
       rotation: new Euler(),
       width: 300,
       height: 150,
+      file: path.join(MODELS_PATH, 'rover.stl')
     };
 
     rovecomm.on('AccelerometerData', (data: number[]) => {
@@ -71,12 +75,14 @@ class Accelerometer extends Component<IProps, IState> {
     });
 
     this.arrowRef = React.createRef();
+    setTimeout(()=>{this.showUpdate = true; this.forceUpdate()}, 30000);
   }
 
-  componentDidMount(): void {
-    if (ROVER_FILE.endsWith('.stl')) {
+  loadGeometry(): void {
+    if (this.state.file.endsWith('.stl')) {
+      this.state.geometry?.dispose();
       const loader = new STLLoader();
-      loader.load(ROVER_FILE, (geo) => {
+      loader.load(this.state.file, (geo) => {
         geo.center();
         geo.computeVertexNormals();
         this.setState({ geometry: geo });
@@ -84,23 +90,31 @@ class Accelerometer extends Component<IProps, IState> {
     }
   }
 
-  componentWillUnmount(): void {
-    //this.state.scene.dispose();
-    //this.state.geometry?.dispose();
-    this.setState({ scene: undefined, geometry: undefined });
+  componentDidMount(): void {
+    this.loadGeometry();
   }
 
+  componentDidUpdate(prevProps: Readonly<IProps>, prevState: Readonly<IState>, snapshot?: any): void {
+    if (prevState.file !== this.state.file) {
+      this.loadGeometry();
+    }
+  }
+
+  componentWillUnmount(): void {
+    this.state.geometry?.dispose();
+  }
+
+  // We must rotate the rover such that downVector would point to <0, -1, 0> if it underwent the same rotation. 
+  // We find the axis to rotate it around by crossing downVector with <0, -1, 0>,
+  // We use a quaternion to represent the axis and angle, then convert it to euler angles.
   calcRotation(): void {
-    const v = this.state.downVector.clone();
-    v.normalize();
-    v.negate();
-    const pitch = Math.atan2(v.y, v.z) - Math.PI * 0.5;
-    const yaw = 0;
-    const roll = Math.atan2(v.y, v.x) - Math.PI * 0.5;
-    const e = new Euler(pitch, yaw, roll, 'XZY');
-    //console.log(e, v);
+    const normalizedDown = this.state.downVector.clone().normalize();
+    const axisAround = normalizedDown.clone().cross(negativeY);
+    const angleAround = normalizedDown.angleTo(negativeY);
+    const q = new Quaternion().setFromAxisAngle(axisAround, angleAround);
+    const e = new Euler().setFromQuaternion(q);
     this.setState({ rotation: e });
-    this.arrowRef.current?.setDirection(this.state.downVector.clone().normalize());
+    this.arrowRef.current?.setDirection(normalizedDown);
   }
 
   render(): JSX.Element {
@@ -118,24 +132,30 @@ class Accelerometer extends Component<IProps, IState> {
             >
               <directionalLight position-y={2} intensity={Math.PI * 0.5} castShadow />
               <ambientLight intensity={0.1 * Math.PI} />
-              {ROVER_FILE.endsWith('.glb') ? (
+              <group>
+                <mesh position={[0, 3, 0]}>
+                  <boxGeometry />
+                  <meshBasicMaterial color={'black'}/>
+                </mesh>
+                <arrowHelper
+                  args={[this.state.downVector, new Vector3(0, 3, 0), 2, 'green', 0.3, 0.3]}
+                  ref={this.arrowRef}
+                />
+              </group>
+              {this.state.file.endsWith('.glb') ? (
                 <RoverScene rotation={this.state.rotation} position-y={-1} scale={2} />
               ) : (
                 <group>
-                  <mesh geometry={this.state.geometry} rotation={this.state.rotation} scale={0.1} castShadow>
+                  <mesh geometry={this.state.geometry} rotation={this.state.rotation} scale={0.08} castShadow>
                     <meshLambertMaterial color={'#B92C2C'} />
                   </mesh>
-                  <arrowHelper
-                    args={[this.state.downVector, new Vector3(), 2, 'green', 0.3, 0.3]}
-                    ref={this.arrowRef}
-                  />
                 </group>
               )}
               <mesh position-y={-3} rotation-x={-Math.PI * 0.5} scale={50} receiveShadow>
                 <planeGeometry />
                 <meshStandardMaterial color={'#c1c1c1'} />
               </mesh>
-              <OrbitControls panSpeed={0} />
+              <OrbitControls enablePan={false} minDistance={3} maxDistance={8} />
             </Suspense>
           </Canvas>
         </div>
@@ -194,6 +214,15 @@ class Accelerometer extends Component<IProps, IState> {
           />
           <div>{`downVec: <${String(Object.values(this.state.downVector))}>`}</div>
         </div>
+        { this.state.file.endsWith('.stl') && this.showUpdate &&
+          <div 
+            style={{backgroundColor: 'lightgreen', padding: '10px', border: '2px solid green', borderRadius: '5px', cursor: 'pointer', fontFamily: 'Comic Sans MS', fontSize: '.7em'}}
+            onClick={() => this.setState({file: path.join(MODELS_PATH, 'rover_preview.glb')})}  
+          >
+            <span>Hey! 3D Rover got an upgrade! 👀</span><br/>
+            <span>Click <strong>here</strong> to check our our new .GLTF support!</span>
+          </div>
+        }
       </div>
     );
   }
