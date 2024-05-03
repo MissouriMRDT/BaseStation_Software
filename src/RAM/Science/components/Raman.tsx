558ee4474459529ccac6d98a111d17f4b46940ea
Will Weidler <weidlerwill@gmail.com>
2024-05-02 19:38:21 -0500

    Implement LED control and graph scaling

--- src/RAM/Science/components/Raman.tsx
+++ src/RAM/Science/components/Raman.tsx
@@ -6,6 +6,20 @@ import fs from 'fs';
 import { rovecomm } from '../../../Core/RoveProtocol/Rovecomm';
 import { windows } from '../../../Core/Window';
 
+const minWavelength = 533;
+const maxWavelength = 650;
+const maxintegrationTime = 60000;
+
+const label: CSS.Properties = {
+  marginTop: '-10px',
+  position: 'relative',
+  top: '24px',
+  fontFamily: 'arial',
+  fontSize: '16px',
+  zIndex: 1,
+  color: 'white',
+};
+
 const container: CSS.Properties = {
   display: 'flex',
   flexDirection: 'column',
@@ -17,31 +31,7 @@ const container: CSS.Properties = {
   height: 'calc(100% - 40px)',
   padding: '5px',
 };
-const label: CSS.Properties = {
-  marginTop: '-10px',
-  position: 'relative',
-  top: '24px',
-  left: '3px',
-  fontFamily: 'arial',
-  fontSize: '16px',
-  zIndex: 1,
-  color: 'white',
-};
-const row: CSS.Properties = {
-  display: 'flex',
-  flexDirection: 'row',
-  flexGrow: 1,
-  justifyContent: 'space-around',
-  alignContent: 'center',
-  marginTop: '5px',
-  width: '100%',
-};
-const column: CSS.Properties = {
-  display: 'flex',
-  flexDirection: 'column',
-  flexGrow: 1,
-  justifyContent: 'space-around',
-};
+
 const componentBox: CSS.Properties = {
   margin: '3px 0 3px 0',
 };
@@ -50,16 +40,41 @@ const overlay: CSS.Properties = {
   width: '200px',
   color: 'black',
 };
-const minWavelength = 400;
-const maxWavelength = 700;
-const maxintegrationTime = 60000;
+
+const buttonRow: CSS.Properties = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-evenly',
+  alignItems: 'center',
+};
+
+const inputField: CSS.Properties = {
+  marginLeft: '5px',
+};
+
+const LEDNames = ['Laser', 'White LED'];
+interface IProps {
+  style?: CSS.Properties;
+}
+
+interface IState {
+  packetsRecieved: boolean[];
+  data: number[];
+  LedStatus: boolean[];
+  graphData: {
+    x: number;
+    y: number;
+  }[];
+  crosshairPos: number | null;
+  enableLED: boolean;
+  integrationTime: number;
+  minX: number;
+  maxX: number;
+  enableLEDToggle: boolean;
+}
 
 function downloadURL(imgData: string): void {
-  const filename = `./Screenshots/${new Date()
-    .toISOString()
-    // ISO string will be fromatted YYYY-MM-DDTHH:MM:SS:sssZ
-    // this regex will convert all -,T,:,Z to . (which covers to . for .csv)
-    .replaceAll(/[:\-TZ]/g, '.')}Raman.png`;
+  const filename = `./Screenshots/${new Date().toISOString().replaceAll(/[:\-TZ]/g, '.')}Raman.png`;
 
   if (!fs.existsSync('./Screenshots')) {
     fs.mkdirSync('./Screenshots');
@@ -70,28 +85,24 @@ function downloadURL(imgData: string): void {
 }
 
 function saveImage(): void {
-  // Search through all the windows for Raman
   let graph;
   let thisWindow;
   for (const win of Object.keys(windows)) {
     if (windows[win].document.getElementById('Raman')) {
-      // When found, store the graph and the window it was in
       thisWindow = windows[win];
       graph = thisWindow.document.getElementById('Raman');
       break;
     }
   }
 
-  // If the graph isn't found, throw an error
   if (!graph) {
     throw new Error("The element 'Raman' wasn't found");
   }
 
-  // If the graph is found, convert its html into a canvas to be downloaded
   html2canvas(graph, {
     scrollX: 0,
     scrollY: -thisWindow.scrollY - 38,
-  }) // We subtract 38 to make up for the 28 pixel top border and the -10 top margin
+  })
     .then((canvas: any) => {
       const imgData = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
       downloadURL(imgData);
@@ -102,54 +113,36 @@ function saveImage(): void {
     });
 }
 
-interface IProps {
-  style?: CSS.Properties;
-}
-
-interface IState {
-  /** array to store which packets have been recieved. There are 5 packets of ccd information since RoveComm has a limited packet size.
-   *  We need to wait until we get all of them before we can do processing.
-   */
-  packetsRecieved: boolean[];
-
-  data: number[];
-
-  graphData: {
-    x: number;
-    y: number;
-  }[];
-
-  crosshairPos: number | null;
-
-  enableLED: boolean;
-  integrationTime: number;
-}
-
-// const LEDWavelength = 532; //nm
-
 class Raman extends Component<IProps, IState> {
   static defaultProps = {
     style: {},
   };
 
+  static buildLedCommand(LED: boolean[]): number {
+    let bitmask = '';
+
+    bitmask += LED[1] ? '1' : '0';
+    bitmask += LED[0] ? '1' : '0';
+    console.log(bitmask);
+    const num = parseInt(bitmask, 2);
+    console.log(num);
+    return num;
+  }
+
   constructor(props: IProps) {
     super(props);
     this.state = {
+      LedStatus: [false, false],
       packetsRecieved: [false, false, false, false, false],
       data: new Array(2048).fill(0).flat(),
       graphData: [{ x: 0, y: 0 }],
-
       crosshairPos: null,
-
       enableLED: false,
       integrationTime: 0,
+      minX: 533,
+      maxX: 650,
+      enableLEDToggle: false,
     };
-    // this.exportData = this.exportData.bind(this);
-    this.processReading = this.processReading.bind(this);
-    this.onNearestX = this.onNearestX.bind(this);
-    this.onMouseLeave = this.onMouseLeave.bind(this);
-    this.requestData = this.requestData.bind(this);
-    this.integrationTimeChange = this.integrationTimeChange.bind(this);
 
     rovecomm.on('RamanReading_Part1', (data: number[]) => this.processReading(1, 0, 500, data));
     rovecomm.on('RamanReading_Part2', (data: number[]) => this.processReading(2, 500, 1000, data));
@@ -159,11 +152,10 @@ class Raman extends Component<IProps, IState> {
   }
 
   processReading(packetID: number, startIndex: number, endIndex: number, data: number[]) {
-    console.log(`processing Raman data of packet ID: ${packetID}`);
     this.setState(
       (prevState) => {
         const updatedPacketsRecieved = prevState.packetsRecieved;
-        updatedPacketsRecieved[packetID - 1] = true; //the -1 term is because packetID index starts at 1
+        updatedPacketsRecieved[packetID - 1] = true;
         const updatedData = prevState.data;
         for (let i = 0; i < endIndex - startIndex; i++) {
           updatedData[startIndex + i] = data[i];
@@ -172,31 +164,25 @@ class Raman extends Component<IProps, IState> {
         return { packetsRecieved: updatedPacketsRecieved, data: updatedData };
       },
       () => {
-        //if all the packets have been recieved, update graph
         let a = true;
         this.state.packetsRecieved.forEach((packetRecieved) => {
           a &&= packetRecieved;
         });
 
-        if (a) this.updateGraphValues(); //TODO there's gotta be a better way, this is cursed
+        if (a) this.updateGraphValues();
       }
     );
   }
 
-  static sendLEDCommand(enable: boolean): number {
-    let bitmask = '';
-    bitmask += enable ? '1' : '0';
-    bitmask += '0'; //for deprecated red LED
-    console.log(bitmask);
-    const num = parseInt(bitmask, 2);
-    console.log(num);
-    return num;
-  }
-
-  setLED(enable: boolean) {
-    this.setState({ enableLED: enable }, () => {
-      rovecomm.sendCommand('EnableLEDs', 'Instruments', Raman.sendLEDCommand(enable));
-    });
+  toggleLed(index: number): void {
+    if (this.state.enableLEDToggle) {
+      const { LedStatus } = this.state;
+      LedStatus[index] = !LedStatus[index];
+      this.setState({
+        LedStatus,
+      });
+      rovecomm.sendCommand('EnableLEDs', 'Instruments', Raman.buildLedCommand(LedStatus));
+    }
   }
 
   onMouseLeave(): void {
@@ -210,7 +196,6 @@ class Raman extends Component<IProps, IState> {
   updateGraphValues(): void {
     const data = this.state.data;
     const xScale = (maxWavelength - minWavelength) / 2048;
-    //TODO do the map from ccd pixel to wavelength here, as well as normalizing intensity of wavelengths from ccd datasheet
     const dataToDisplay = data.map((value: number, index: number) => {
       return { x: index * xScale + minWavelength, y: value };
     });
@@ -223,16 +208,11 @@ class Raman extends Component<IProps, IState> {
   crosshair(): JSX.Element | null {
     const { crosshairPos } = this.state;
 
-    // If we were able to find a reading at a time, then go ahead and display the crosshair
-    // The heading will be that time as a string, and then if the key exists in crosshairValues
-    // then we want to display its y value
     if (crosshairPos) {
       return (
         <Crosshair values={[this.state.graphData[crosshairPos]]}>
           <div style={overlay}>
-            <h3 style={{ borderStyle: 'solid', width: '30%', textAlign: 'center', backgroundColor: 'white' }}>
-              {this.state.graphData[crosshairPos].y}
-            </h3>
+            <h3>{this.state.graphData[crosshairPos].y}</h3>
           </div>
         </Crosshair>
       );
@@ -243,13 +223,11 @@ class Raman extends Component<IProps, IState> {
   requestData(): void {
     this.setState(() => {
       return {
-        //reset stored data to prepare for new aquisitions
         packetsRecieved: new Array(5).fill(false),
       };
     });
 
     rovecomm.sendCommand('RequestRamanReading', 'Instruments', this.state.integrationTime);
-    console.log(`Requesting Raman Reading`);
   }
 
   integrationTimeChange(event: { target: { value: string } }): void {
@@ -273,8 +251,8 @@ class Raman extends Component<IProps, IState> {
               margin={{ top: 10, bottom: 50 }}
               width={window.document.documentElement.clientWidth - 50}
               height={300}
-              yDomain={[0, 1023]} //TODO determine these values
-              xDomain={[minWavelength, maxWavelength]}
+              yDomain={[0, 1023]}
+              xDomain={[this.state.minX, this.state.maxX]}
             >
               <VerticalGridLines style={{ fill: 'none' }} />
               <HorizontalGridLines style={{ fill: 'none' }} />
@@ -289,34 +267,64 @@ class Raman extends Component<IProps, IState> {
               <YAxis />
               {this.crosshair()}
             </XYPlot>
-            <div style={{ ...row }}>
-              <div style={{ ...row, justifyContent: 'center' }}>
-                <div>
-                  <button onClick={() => this.setLED(!this.state.enableLED)}>
-                    LED: {this.state.enableLED ? 'on' : 'off'}
-                  </button>
-                </div>
-                <div>
-                  Integration Time (ms):
-                  <input
-                    type="text"
-                    style={{ marginLeft: '5px' }}
-                    value={this.state.integrationTime || ''}
-                    onChange={this.integrationTimeChange}
-                  />
-                </div>
-                <div>
-                  <button
-                    onClick={() => {
-                      this.requestData(); //TODO make a counter variable or a text box to tweak the number of aquisitions
-                    }}
-                  >
-                    Request Reading
-                  </button>
-                </div>
-                <div>
-                  <button onClick={saveImage}>Export Graph</button>
-                </div>
+            <div style={buttonRow}>
+              <div>
+                {this.state.LedStatus.map((value, index) => {
+                  const uniqueKey = `ledToggle_${index}`;
+                  return (
+                    <label key={uniqueKey} htmlFor={uniqueKey}>
+                      <input
+                        type="checkbox"
+                        id={uniqueKey}
+                        name={uniqueKey}
+                        checked={value}
+                        onChange={() => this.toggleLed(index)}
+                      />
+                      {LEDNames[index]}
+                    </label>
+                  );
+                })}
+                <button onClick={() => this.setState((prevState) => ({ enableLEDToggle: !prevState.enableLEDToggle }))}>
+                  LED Toggle: {this.state.enableLEDToggle ? 'on' : 'off'}
+                </button>
+              </div>
+              <div>
+                Integration Time (ms):
+                <input
+                  type="text"
+                  style={inputField}
+                  value={this.state.integrationTime || ''}
+                  onChange={(e) => this.integrationTimeChange(e)}
+                />
+              </div>
+              <div>
+                <button onClick={() => this.requestData()}>Request Reading</button>
+              </div>
+            </div>
+            <div style={buttonRow}>
+              <div>
+                Min X:
+                <input
+                  type="text"
+                  style={inputField}
+                  value={this.state.minX || ''}
+                  onChange={(e) => this.setState({ minX: parseInt(e.target.value) })}
+                />
+              </div>
+              <div>
+                Max X:
+                <input
+                  type="text"
+                  style={inputField}
+                  value={this.state.maxX || ''}
+                  onChange={(e) => this.setState({ maxX: parseInt(e.target.value) })}
+                />
+              </div>
+              <div>
+                <button onClick={() => this.setState({ minX: 533, maxX: 650 })}>Reset Graph</button>
+              </div>
+              <div>
+                <button onClick={saveImage}>Export Graph</button>
               </div>
             </div>
           </div>
