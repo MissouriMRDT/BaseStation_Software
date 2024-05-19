b32a3e495373f219c1913a3c5480e42df5b3a42c
Will Weidler <weidlerwill@gmail.com>
2024-05-19 15:28:01 -0500

    Added arm slider visual and editor

--- src/RAM/Arm/Arm.tsx
+++ src/RAM/Arm/Arm.tsx
@@ -12,29 +12,64 @@ const row: CSS.Properties = {
   flexGrow: 1,
   justifyContent: 'space-between',
 };
+const h1Style: CSS.Properties = {
+  fontFamily: 'arial',
+  fontSize: '12px',
+};
+const input: CSS.Properties = {
+  width: '75%',
+};
+const container: CSS.Properties = {
+  display: 'flex',
+  flexDirection: 'column',
+  fontFamily: 'arial',
+  borderTopWidth: '28px',
+  borderColor: '#990000',
+  borderBottomWidth: '2px',
+  borderStyle: 'solid',
+};
+const label: CSS.Properties = {
+  marginTop: '-10px',
+  position: 'relative',
+  top: '24px',
+  left: '3px',
+  fontFamily: 'arial',
+  fontSize: '16px',
+  zIndex: 1,
+  color: 'white',
+};
+const joints: CSS.Properties = {
+  display: 'grid',
+  gridRowStart: '2 & {}',
+  grid: 'repeat(2, 28px) / auto-flow dense',
+  margin: '10px 10px',
+};
 const column: CSS.Properties = {
   display: 'flex',
   flexDirection: 'column',
   flexGrow: 1,
 };
 
+interface Joint {
+  [key: string]: number;
+  X: number;
+  Y1: number;
+  Y2: number;
+  Z: number;
+  Pitch: number;
+  R: number;
+  Gripper: number;
+  Master: number;
+}
+
 interface IProps {}
 
 interface IState {
+  multiplierValues: Joint;
   gripperToggle: boolean;
   gripperCam: number;
   laserToggle: boolean;
 }
-
-let MultiplierX = 1000;
-let MultiplierY1 = 1000;
-let MultiplierY2 = 1000;
-let MultiplierZ = 1000;
-let MultiplierPitch = 500;
-let MultiplierR = 500;
-let MultiplierGripper = 1000;
-let MultiplerMaster = 1;
-// let MultiplierEndEffector: number;
 
 class Arm extends Component<IProps, IState> {
   constructor(props: IProps) {
@@ -43,13 +78,35 @@ class Arm extends Component<IProps, IState> {
       gripperToggle: false,
       gripperCam: 6,
       laserToggle: false,
+      multiplierValues: {
+        X: 1000,
+        Y1: 1000,
+        Y2: 1000,
+        Z: 1000,
+        Pitch: 500,
+        R: 500,
+        Gripper: 1000,
+        Master: 1,
+      },
     };
     this.setGripper = this.setGripper.bind(this);
     this.arm = this.arm.bind(this);
+    this.setLaser = this.setLaser.bind(this);
+    this.handleMultiplierChange = this.handleMultiplierChange.bind(this);
     setInterval(() => this.arm(), 100);
   }
 
   isGripperToggling = false;
+
+  handleMultiplierChange(joint: string, e: React.ChangeEvent<HTMLInputElement>) {
+    const value = Number(e.target.value);
+    this.setState((prevState) => ({
+      multiplierValues: {
+        ...prevState.multiplierValues,
+        [joint]: value,
+      },
+    }));
+  }
 
   setGripper() {
     if (this.isGripperToggling) {
@@ -57,19 +114,14 @@ class Arm extends Component<IProps, IState> {
     }
 
     this.isGripperToggling = true;
-
-    this.setState((prevState) => ({ gripperToggle: !prevState.gripperToggle }));
     rovecomm.sendCommand('SelectGripper', 'Arm', this.state.gripperToggle ? [1] : [0]);
     if (!this.state.gripperToggle) {
       this.setState({ gripperCam: 6 });
       console.log(this.state.gripperCam);
-      MultiplierR = 500;
     } else {
       this.setState({ gripperCam: 7 });
       console.log(this.state.gripperCam);
-      MultiplierR = 1000;
     }
-
     setTimeout(() => {
       this.isGripperToggling = false;
     }, 300); // 300ms delay
@@ -102,30 +154,49 @@ class Arm extends Component<IProps, IState> {
     let Gripper = 0;
     let moveArm = false;
 
-    if (controllerInputs.MultiplierY) {
-      MultiplierX = (MultiplerMaster * (controllerInputs.Multiplier1 + 1)) / 0.002;
-      MultiplierY1 = (MultiplerMaster * (controllerInputs.Multiplier2 + 1)) / 0.002;
-      MultiplierY2 = (MultiplerMaster * (controllerInputs.Multiplier3 + 1)) / 0.002;
-      MultiplierZ = (MultiplerMaster * (controllerInputs.Multiplier4 + 1)) / 0.002;
-    } else if (controllerInputs.MultiplierX) {
-      MultiplierPitch = (MultiplerMaster * (controllerInputs.Multiplier1 + 1)) / 0.002;
-      MultiplierR = (MultiplerMaster * (controllerInputs.Multiplier2 + 1)) / 0.002;
-      MultiplierGripper = (MultiplerMaster * (controllerInputs.Multiplier3 + 1)) / 0.002;
-      MultiplerMaster = (controllerInputs.Multiplier4 + 1) / 2;
+    if (
+      'MultiplierY' in controllerInputs &&
+      'MultiplierX' in controllerInputs &&
+      'Multiplier1' in controllerInputs &&
+      'Multiplier2' in controllerInputs &&
+      'Multiplier3' in controllerInputs &&
+      'Multiplier4' in controllerInputs
+    ) {
+      if (controllerInputs.MultiplierY) {
+        this.setState((prevState) => ({
+          multiplierValues: {
+            ...prevState.multiplierValues,
+            X: (prevState.multiplierValues.Master * (controllerInputs.Multiplier1 + 1)) / 0.002,
+            Y1: (prevState.multiplierValues.Master * (controllerInputs.Multiplier2 + 1)) / 0.002,
+            Y2: (prevState.multiplierValues.Master * (controllerInputs.Multiplier3 + 1)) / 0.002,
+            Z: (prevState.multiplierValues.Master * (controllerInputs.Multiplier4 + 1)) / 0.002,
+          },
+        }));
+      } else if (this.state.multiplierValues.X) {
+        this.setState((prevState) => ({
+          multiplierValues: {
+            ...prevState.multiplierValues,
+            Pitch: (prevState.multiplierValues.Master * (controllerInputs.Multiplier1 + 1)) / 0.002,
+            R: (prevState.multiplierValues.Master * (controllerInputs.Multiplier2 + 1)) / 0.002,
+            Gripper: (prevState.multiplierValues.Master * (controllerInputs.Multiplier3 + 1)) / 0.002,
+            Master: (controllerInputs.Multiplier4 + 1) / 2,
+          },
+        }));
+      }
     }
     console.log(
-      MultiplierX,
-      MultiplierY1,
-      MultiplierY2,
-      MultiplierZ,
-      MultiplierPitch,
-      MultiplierR,
-      MultiplierGripper,
-      MultiplerMaster
+      this.state.multiplierValues.X,
+      this.state.multiplierValues.Y1,
+      this.state.multiplierValues.Y2,
+      this.state.multiplierValues.Z,
+      this.state.multiplierValues.Pitch,
+      this.state.multiplierValues.R,
+      this.state.multiplierValues.Gripper,
+      this.state.multiplierValues.Master
     );
 
     if ('WristPitchPlus' in controllerInputs && 'WristPitchMinus' in controllerInputs) {
-      Pitch = (controllerInputs.WristPitchPlus - controllerInputs.WristPitchMinus) * MultiplierPitch;
+      Pitch = (controllerInputs.WristPitchPlus - controllerInputs.WristPitchMinus) * this.state.multiplierValues.Pitch;
       moveArm = true;
     }
 
@@ -136,24 +207,24 @@ class Arm extends Component<IProps, IState> {
     }
 
     if ('RollPlus' in controllerInputs && 'RollMinus' in controllerInputs) {
-      R = (controllerInputs.RollPlus - controllerInputs.RollMinus) * MultiplierR;
+      R = (controllerInputs.RollPlus - controllerInputs.RollMinus) * this.state.multiplierValues.R;
       moveArm = true;
     }
 
     if ('XAxis' in controllerInputs) {
-      X = controllerInputs.XAxis * MultiplierX;
+      X = controllerInputs.XAxis * this.state.multiplierValues.X;
       moveArm = true;
     }
     if ('Y1Axis' in controllerInputs) {
-      Y1 = controllerInputs.Y1Axis * MultiplierY1;
+      Y1 = controllerInputs.Y1Axis * this.state.multiplierValues.Y1;
       moveArm = true;
     }
     if ('Y2Axis' in controllerInputs) {
-      Y2 = controllerInputs.Y2Axis * MultiplierY2;
+      Y2 = controllerInputs.Y2Axis * this.state.multiplierValues.Y2;
       moveArm = true;
     }
     if ('ZAxis' in controllerInputs) {
-      Z = controllerInputs.ZAxis * MultiplierZ;
+      Z = controllerInputs.ZAxis * this.state.multiplierValues.Z;
       moveArm = true;
     }
 
@@ -177,9 +248,9 @@ class Arm extends Component<IProps, IState> {
 
     if ('GripperOpen' in controllerInputs && 'GripperClose' in controllerInputs) {
       if (controllerInputs.GripperOpen === 1) {
-        Gripper = 1 * MultiplierGripper;
+        Gripper = 1 * this.state.multiplierValues.Gripper;
       } else if (controllerInputs.GripperClose === 1) {
-        Gripper = -1 * MultiplierGripper;
+        Gripper = -1 * this.state.multiplierValues.Gripper;
       } else {
         Gripper = 0;
       }
@@ -219,6 +290,26 @@ class Arm extends Component<IProps, IState> {
         </div>
         <div style={row}>
           <ControlScheme configs={['Arm']} style={{ width: '100%', marginRight: '2.5px' }} />
+        </div>
+        <div style={label}>Slider Speeds</div>
+        <div style={container}>
+          <div style={joints}>
+            {Object.keys(this.state.multiplierValues).map((joint) => {
+              return (
+                <div key={joint} style={row}>
+                  <h1 style={h1Style}>{joint}</h1>
+                  <input
+                    type="text"
+                    style={input}
+                    value={this.state.multiplierValues[joint] || ''}
+                    onChange={(e) => {
+                      this.handleMultiplierChange(joint, e);
+                    }}
+                  />
+                </div>
+              );
+            })}
+          </div>{' '}
         </div>
         <CameraControls
           startSource={this.state.gripperCam - 1}
